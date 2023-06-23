294d1b512f11e140ece65aaaa7aaf9d0ad816dc5
Diego Cohen <diego.cohen3@gmail.com>
2023-06-23 13:12:29 -0400

    Initialize repo with typescript and eslint

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>NYPL Research Catalog</title>
+        <meta name="description" content="NYPL Research Catalog" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>NYPL Research Catalog</h1>
+      </main>
+    </>
+  )
+}
