5b3afc175e37d9451af5949e646cb4aaea040941
Diego Cohen <diego.cohen3@gmail.com>
2023-06-26 14:37:23 -0400

    Remove meta tags from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,6 @@ export default function Home() {
     <>
       <Head>
         <title>NYPL Research Catalog</title>
-        <meta name="description" content="NYPL Research Catalog" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
         <h1>NYPL Research Catalog</h1>
