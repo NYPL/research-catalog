3dd3258c6aa4fff052f13a1fa997235fc7846e19
Diego Cohen <diego.cohen3@gmail.com>
2023-06-26 11:53:10 -0400

    Move head to _document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>NYPL Research Catalog</title>
+        <meta name="description" content="NYPL Research Catalog" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,6 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>NYPL Research Catalog</title>
-        <meta name="description" content="NYPL Research Catalog" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <main>
         <h1>NYPL Research Catalog</h1>
       </main>
