2f98b8ba33822e9b6bebffcc0ec0b25249b4d977
Diego Cohen <diegocohen@nypl.org>
2023-07-10 15:10:50 -0400

    Add max width container to main content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,16 @@ function App({Component, pageProps}) {
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
       {/* NYPL Header script and container */}
-      <Script strategy="lazyOnload" src={`${process.env.NEXT_PUBLIC_NYPL_HEADER_URL}/header.min.js?containerId=nypl-header`}/>
+      <Script strategy="lazyOnload"
+              src={`${process.env.NEXT_PUBLIC_NYPL_HEADER_URL}/header.min.js?containerId=nypl-header`}/>
       <div id="nypl-header" className={styles.nyplHeader}></div>
-      <Component {...pageProps} />
+
+      <div className={styles.appContainer}>
+        <Component {...pageProps} />
+      </div>
       {/* NYPL Footer script and container */}
-      <Script strategy="lazyOnload" src={`${process.env.NEXT_PUBLIC_NYPL_HEADER_URL}/footer.min.js?containerId=nypl-footer`} />
+      <Script strategy="lazyOnload"
+              src={`${process.env.NEXT_PUBLIC_NYPL_HEADER_URL}/footer.min.js?containerId=nypl-footer`}/>
       <div id="nypl-footer"></div>
     </>
   )
