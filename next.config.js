f72d1e49c5fbabd566b0c8ca4ea96d6d0f871994
Diego Cohen <diego.cohen3@gmail.com>
2023-07-17 10:10:42 -0400

    Render home page content

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/research/research-catalog',
+  basePath: "/research/research-catalog",
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
+import Link from "next/link"
 import Layout from "@/components/Layout/Layout"
-import Search from "@/components/SearchForm/Search"
+import { Heading, Link as DSLink } from "@nypl/design-system-react-components"
 
 export default function Home() {
   return (
@@ -10,7 +11,155 @@ export default function Home() {
       </Head>
       <main>
         <Layout>
-          <></>
+          <>
+            <Heading level="two">
+              Explore the Library&apos;s Vast Research Collections & More
+            </Heading>
+            <div className="nypl-column-full">
+              <p>
+                Discover millions of items from The New York Public
+                Library&apos;s Stephen A. Schwarzman Building, Schomburg Center
+                for Research in Black Culture, and The New York Public Library
+                for the Performing Arts. Plus, access materials from library
+                collections at Columbia University, Harvard University, and
+                Princeton University.{" "}
+                <Link
+                  href="https://www.nypl.org/research/collections/about/shared-collection-catalog"
+                  passHref
+                >
+                  <DSLink>Learn more.</DSLink>
+                </Link>
+              </p>
+              <p>
+                Please note that the Research Catalog does not include
+                circulating materials. For books and more that you can check out
+                to take home please visit our{" "}
+                <Link href="https://browse.nypl.org" passHref>
+                  <DSLink>circulating branch catalog.</DSLink>
+                </Link>{" "}
+                The{" "}
+                <Link href="https://legacycatalog.nypl.org/" passHref>
+                  <DSLink>legacy research catalog</DSLink>
+                </Link>{" "}
+                is still available, but does not include all of our Scan &
+                Deliver options or the Columbia University, Harvard University,
+                and Princeton University material from the Shared Collection.
+              </p>
+            </div>
+            <div>
+              <Heading level="three">Research at NYPL</Heading>
+
+              <div className="nypl-row nypl-quarter-image">
+                <div className="nypl-column-one-quarter">
+                  <img
+                    className="nypl-quarter-image"
+                    src="https://cdn-petrol.nypl.org/sites/default/media/styles/extralarge/public/archives-portal.jpg?itok=-oYtHmeO"
+                    alt=""
+                    role="presentation"
+                  />
+                </div>
+                <div className="nypl-column-three-quarters image-column-three-quarters">
+                  <Heading level="four">
+                    <Link href="/research/collections" passHref>
+                      <DSLink>Collections</DSLink>
+                    </Link>
+                  </Heading>
+                  <p>
+                    Discover our world-renowned research collections, featuring
+                    more than 46 million items.
+                  </p>
+                </div>
+              </div>
+
+              <div className="nypl-row nypl-quarter-image">
+                <div className="nypl-column-one-quarter">
+                  <img
+                    className="nypl-quarter-image"
+                    src="https://cdn-petrol.nypl.org/sites/default/media/styles/extralarge/public/sasb.jpg?itok=sdQBITR7"
+                    alt=""
+                    role="presentation"
+                  />
+                </div>
+                <div className="nypl-column-three-quarters image-column-three-quarters">
+                  <Heading level="four">
+                    <Link href="/locations/map?libraries=research" passHref>
+                      <DSLink>Locations</DSLink>
+                    </Link>
+                  </Heading>
+                  <p>
+                    Access items, one-on-one reference help, and dedicated
+                    research study rooms.
+                  </p>
+                </div>
+              </div>
+
+              <div className="nypl-row nypl-quarter-image">
+                <div className="nypl-column-one-quarter">
+                  <img
+                    className="nypl-quarter-image"
+                    src="https://cdn-petrol.nypl.org/sites/default/media/styles/extralarge/public/divisions.jpg?itok=O4uSedcp"
+                    alt=""
+                    role="presentation"
+                  />
+                </div>
+                <div className="nypl-column-three-quarters image-column-three-quarters">
+                  <Heading level="four">
+                    <Link href="/about/divisions" passHref>
+                      <DSLink>Divisions</DSLink>
+                    </Link>
+                  </Heading>
+                  <p>
+                    Learn about the subject and media specializations of our
+                    research divisions.
+                  </p>
+                </div>
+              </div>
+
+              <div className="nypl-row nypl-quarter-image">
+                <div className="nypl-column-one-quarter">
+                  <img
+                    className="nypl-quarter-image"
+                    src="https://cdn-petrol.nypl.org/sites/default/media/styles/extralarge/public/plan-you-visit.jpg?itok=scG6cFgy"
+                    alt=""
+                    role="presentation"
+                  />
+                </div>
+                <div className="nypl-column-three-quarters image-column-three-quarters">
+                  <Heading level="four">
+                    <Link href="/research/support" passHref>
+                      <DSLink>Support</DSLink>
+                    </Link>
+                  </Heading>
+                  <p>
+                    Plan your in-person research visit and discover resources
+                    for scholars and writers.
+                  </p>
+                </div>
+              </div>
+
+              <div className="nypl-row nypl-quarter-image">
+                <div className="nypl-column-one-quarter">
+                  <img
+                    className="nypl-quarter-image"
+                    src="https://cdn-petrol.nypl.org/sites/default/media/styles/extralarge/public/research-services.jpg?itok=rSo9t1VF"
+                    alt=""
+                    role="presentation"
+                  />
+                </div>
+                <div className="nypl-column-three-quarters image-column-three-quarters">
+                  <Heading level="four">
+                    <Link href="/research/services" passHref>
+                      <DSLink>Services</DSLink>
+                    </Link>
+                  </Heading>
+                  <p>
+                    Explore services for online and remote researchers, as well
+                    as our interlibrary services.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </>
         </Layout>
       </main>
     </>
