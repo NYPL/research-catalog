ae68e409299cd9c6a0b30a27e9c15d8abe103f15
Diego Cohen <diego.cohen3@gmail.com>
2023-07-18 11:20:35 -0400

    Fix type and buffer errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head"
-import nyplApiClient from "@/src/server/nyplApiClient"
 
 export default function Home() {
   return (
@@ -12,20 +11,4 @@ export default function Home() {
       </main>
     </>
   )
-}
-
-export async function getServerSideProps() {
-  await nyplApiClient();
-  await nyplApiClient({ apiName: 'discovery' })
-    .then(client => {
-      return client.get(
-        `/discovery/resources`
-      )
-    })
-    .then(response => console.log({response}));
-  return {
-    props: {
-      data: "data"
-    }
-  }
 }
--- src/server/nyplApiClient/index.ts
+++ src/server/nyplApiClient/index.ts
@@ -1,15 +1,16 @@
-import NyplApiClient from '@nypl/nypl-data-api-client';
-import aws from 'aws-sdk';
+import NyplApiClient from "@nypl/nypl-data-api-client"
+import aws from "aws-sdk"
+import { string } from "prop-types"
 
 const config: any = {
   api: {
     platform: {
       development:
         process.env.PLATFORM_API_BASE_URL ||
-        'https://qa-platform.nypl.org/api/v0.1',
+        "https://qa-platform.nypl.org/api/v0.1",
       production:
         process.env.PLATFORM_API_BASE_URL ||
-        'https://platform.nypl.org/api/v0.1',
+        "https://platform.nypl.org/api/v0.1",
     },
     // The 'discovery' base URL should use DISCOVERY_API_BASE_URL if set,
     // falling back on PLATFORM_API_BASE_URL if set,
@@ -18,68 +19,70 @@ const config: any = {
       development:
         process.env.DISCOVERY_API_BASE_URL ||
         process.env.PLATFORM_API_BASE_URL ||
-        'https://qa-platform.nypl.org/api/v0.1',
+        "https://qa-platform.nypl.org/api/v0.1",
       production:
         process.env.DISCOVERY_API_BASE_URL ||
         process.env.PLATFORM_API_BASE_URL ||
-        'https://platform.nypl.org/api/v0.1',
+        "https://platform.nypl.org/api/v0.1",
     },
     drbb: {
       development:
-        process.env.DRB_API_BASE_URL || 'http://drb-api-qa.nypl.org/search/',
+        process.env.DRB_API_BASE_URL || "http://drb-api-qa.nypl.org/search/",
       production:
         process.env.DRB_API_BASE_URL ||
-        'https://digital-research-books-api.nypl.org/v3/sfr/search',
+        "https://digital-research-books-api.nypl.org/v3/sfr/search",
     },
   },
-  tokenUrl: 'https://isso.nypl.org/',
-};
-const appEnvironment = process.env.APP_ENV || 'production';
-const kmsEnvironment = process.env.KMS_ENV || 'encrypted';
-let decryptKMS;
-let kms;
+  tokenUrl: "https://isso.nypl.org/",
+}
+const appEnvironment = process.env.APP_ENV || "production"
+const kmsEnvironment = process.env.KMS_ENV || "encrypted"
+let decryptKMS
+let kms
 
-if (kmsEnvironment === 'encrypted') {
+if (kmsEnvironment === "encrypted") {
   kms = new aws.KMS({
-    region: 'us-east-1',
-  });
+    region: "us-east-1",
+  })
   // console.log({kms})
 
-  decryptKMS = (key) => {
+  decryptKMS = async (key: string) => {
+    console.log("Decrypt func INPUT: " + key)
     const params = {
-      CiphertextBlob: new Buffer(key, 'base64'),
-    };
-    console.log({key, params})
+      CiphertextBlob: Buffer.from(key, "base64"),
+    }
+    const { Plaintext } = await kms.decrypt(params).promise()
 
-    return new Promise((resolve, reject) => {
+    return await new Promise((resolve, reject) => {
       kms.decrypt(params, (err, data) => {
         if (err) {
-          console.log({err})
-          reject(err);
+          console.log({ err })
+          reject(err)
         } else {
-          resolve(data.Plaintext.toString());
+          resolve(Plaintext.toString())
         }
-      });
-    });
-  };
+      })
+    })
+  }
 }
 
-const clientId = process.env.clientId || process.env.PLATFORM_API_CLIENT_ID;
-const clientSecret = process.env.clientSecret || process.env.PLATFORM_API_CLIENT_SECRET;
+const clientId = process.env.clientId || process.env.PLATFORM_API_CLIENT_ID
+const clientSecret =
+  process.env.clientSecret || process.env.PLATFORM_API_CLIENT_SECRET
 
-const keys = [clientId, clientSecret];
-const CACHE = { clients: [] };
+const keys = [clientId, clientSecret]
+const CACHE = { clients: [], clientSecret: string, clientId: string }
 
-function nyplApiClient(options = { apiName: 'platform' }) {
-  const { apiName } = options;
+const nyplApiClient = async (options = { apiName: "platform" }) => {
+  const { apiName } = options
   if (CACHE.clients[apiName]) {
-    return Promise.resolve(CACHE.clients[apiName]);
+    return await Promise.resolve(CACHE.clients[apiName])
   }
 
-  const baseUrl = config.api[apiName][appEnvironment];
+  const baseUrl = config.api[apiName][appEnvironment]
 
-  if (kmsEnvironment === 'encrypted') {
-    return new Promise((resolve, reject) => {
+  if (kmsEnvironment === "encrypted") {
+    return await new Promise((resolve, reject) => {
       Promise.all(keys.map(decryptKMS))
         .then(([decryptedClientId, decryptedClientSecret]) => {
           const nyplApiClient = new NyplApiClient({
@@ -87,20 +90,20 @@ function nyplApiClient(options = { apiName: 'platform' }) {
             oauth_key: decryptedClientId,
             oauth_secret: decryptedClientSecret,
             oauth_url: config.tokenUrl,
-          });
+          })
 
-          CACHE.clientId = clientId;
-          CACHE.clientSecret = clientSecret;
-          CACHE.clients[apiName] = nyplApiClient;
+          CACHE.clientId = clientId
+          CACHE.clientSecret = clientSecret
+          CACHE.clients[apiName] = nyplApiClient
 
-          resolve(nyplApiClient);
+          resolve(nyplApiClient)
         })
         .catch((error) => {
           // TODO: add this once the Winston config is added.
           // logger.error('ERROR trying to decrypt using KMS.', error);
-          reject('ERROR trying to decrypt using KMS.', error);
-        });
-    });
+          reject(error)
+        })
+    })
   }
 
   const nyplApiClient = new NyplApiClient({
@@ -108,13 +111,13 @@ function nyplApiClient(options = { apiName: 'platform' }) {
     oauth_key: clientId,
     oauth_secret: clientSecret,
     oauth_url: config.tokenUrl,
-  });
+  })
 
-  CACHE.clientId = clientId;
-  CACHE.clientSecret = clientSecret;
-  CACHE.clients[apiName] = nyplApiClient;
+  CACHE.clientId = clientId
+  CACHE.clientSecret = clientSecret
+  CACHE.clients[apiName] = nyplApiClient
 
-  return Promise.resolve(nyplApiClient);
+  return await Promise.resolve(nyplApiClient)
 }
 
-export default nyplApiClient;
+export default nyplApiClient
