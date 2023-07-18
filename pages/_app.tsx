20679ee7531fcfb54c1f81fb3b809a36593a6e39
Diego Cohen <diego.cohen3@gmail.com>
2023-07-18 16:08:55 -0400

    Use router to show searchBar on home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head"
 import Layout from "@/components/Layout/Layout"
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps, router }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Layout>
+      <Layout showSearch={router.pathname === "/"}>
         <Component {...pageProps} />
       </Layout>
     </>
