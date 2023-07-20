07e3f71a160356246c2f9e98b2c5472813a7795c
Diego Cohen <diego.cohen3@gmail.com>
2023-07-20 15:30:04 -0400

    Add comment above getActivePage util

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,5 +1,11 @@
 import type { RCPage } from "@/config/types"
 
+/**
+ * getActivePage(pathname)
+ * Returns the Research Catalog page ID for a given pathname.
+ * Used for determining the current page for activating menu links and
+ * conditionally rendering the Search form.
+ */
 export const getActivePage = (pathname: string): RCPage => {
   if (pathname === "/") {
     return "search"
