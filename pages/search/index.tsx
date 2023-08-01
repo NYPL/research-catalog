f347b0d1aa3901b4fc09c84eb20fa36282f94cfa
Diego Cohen <diegocohen@nypl.org>
2023-08-01 16:19:18 -0400

    Add temporary client side fetch for search results

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,7 +1,21 @@
 import Head from "next/head"
 import { Heading } from "@nypl/design-system-react-components"
+import { useEffect } from "react"
 
 export default function Search() {
+  function getSearchResults() {
+    fetch("/research/research-catalog/api/search?q=diego")
+      .then(async (response) => await response.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+  useEffect(() => {
+    getSearchResults()
+  }, [])
   return (
     <div style={{ paddingBottom: "var(--nypl-space-l)" }}>
       <Head>
