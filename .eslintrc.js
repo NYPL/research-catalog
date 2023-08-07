ed8c6ab037f37ae99df993cc16b7566f0380dd49
Diego Cohen <diego.cohen3@gmail.com>
2023-08-07 14:35:20 -0400

    Test pre commit config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     node: true,
   },
   extends: [
+    "next/core-web-vitals",
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
