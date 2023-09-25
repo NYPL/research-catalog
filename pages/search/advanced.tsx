41cdf59e32385561bcb6cb8f816b80aa707a8751
Diego Cohen <diego.cohen3@gmail.com>
2023-09-25 16:52:29 -0400

    Add important to margin selector

--- pages/search/advanced.tsx
+++ pages/search/advanced.tsx
@@ -186,7 +186,7 @@ export default function AdvancedSearch() {
                   "grid-template-columns": "repeat(2, minmax(0, 1fr))",
                   "grid-gap": "var(--nypl-space-s)",
                   div: {
-                    marginTop: 0,
+                    marginTop: "0 !important",
                   },
                 },
               }}
