4442a0e01fb3222174c2aa13b6c5d53cad47dcfc
Diego Cohen <diego.cohen3@gmail.com>
2023-09-25 18:01:37 -0400

    Remove console log

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -34,7 +34,6 @@ export default function Search({ results }) {
       return !(isEmpty(result) || (result.result && isEmpty(result.result)))
     })
     .map((result: SearchResultsElement) => {
-      console.log(result.result)
       return new SearchResultsBib(result.result)
     })
 
