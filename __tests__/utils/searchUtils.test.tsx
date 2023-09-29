433ba91bda53e8a27faf7c356a82016d1087fcf2
Vera Kahn <verakahn@nypl.org>
2023-09-29 17:10:06 -0400

    add searchutils tests

---
+++ __tests__/utils/searchUtils.test.tsx
@@ -0,0 +1,52 @@
+import { getQueryString, mapQueryToSearchParams, mapRequestBodyToSearchParams } from '../../src/utils/searchUtils'
+
+describe('searchUtils', () => {
+  describe('getQueryString', () => {
+    it('should create query strings with comma delimited values', () => {
+      expect(getQueryString({
+        q: 'shel silverstein',
+        field: 'contributor'
+      })).toBe('?q=shel%20silverstein&search_scope=contributor')
+    })
+  })
+  describe('mapQueryToSearchParams', () => {
+    it('should consolidate identifiers and change some keys', () => {
+      expect(mapQueryToSearchParams({
+        issn: '123',
+        isbn: '456',
+        search_scope: 'contributor',
+        sort_direction: 'asc',
+        sort: 'spaghetti'
+      })).toEqual({
+        identifiers: {
+          issn: '123',
+          isbn: '456'
+        },
+        field: 'contributor',
+        order: 'asc',
+        sortBy: 'spaghetti'
+      })
+    })
+  })
+  describe('mapRequestBodyToSearchParams', () => {
+    it('combines filters', () => {
+      const params = mapRequestBodyToSearchParams(
+        {
+          q: 'spaghetti',
+          language: 'igbo',
+          materialType: 'scroll',
+          dateAfter: '1900',
+          dateBefore: '1902'
+        })
+      expect(params).toEqual({
+        q: 'spaghetti',
+        filters: {
+          language: 'igbo',
+          materialType: 'scroll',
+          dateAfter: '1900',
+          dateBefore: '1902'
+        }
+      })
+    })
+  })
+})
