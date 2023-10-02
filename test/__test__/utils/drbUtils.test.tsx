3c1deb27e0533ba39ebd8a151da6d6f5e5a47e01
Vera Kahn <verakahn@nypl.org>
2023-10-02 15:56:51 -0400

    rename file

--- test/__test__/utils/drbUtils.test.tsx
+++ test/__test__/utils/drbUtils.test.tsx
@@ -1,6 +1,6 @@
 import { getDRBQueryStringFromSearchParams } from "../../../src/utils/drbUtils"
 import { DRB_RESULTS_PER_PAGE } from "../../../src/config/constants"
-import { queryParamsEquality } from "../../helpers/searchUtils"
+import { queryParamsEquality } from "../../helpers/searchHelpers"
 
 const checkQueryParamsEquality = queryParamsEquality(
   getDRBQueryStringFromSearchParams
