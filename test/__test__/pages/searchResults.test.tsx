d9d7400e0f1ff35074b9f6d7e14f0b27b504ec16
Vera Kahn <verakahn@nypl.org>
2023-10-04 15:31:19 -0400

    search results tests

--- test/__test__/pages/searchResults.test.tsx
+++ test/__test__/pages/searchResults.test.tsx
@@ -3,20 +3,37 @@ import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import mockRouter from "next-router-mock"
 
-import { results } from "../../data/searchResultsBibs"
+import { results } from "../../data/searchResultsManyBibs"
+import { noBibs } from "../../data/searchResultsNoBibs"
 import SearchResults from "../../../pages/search/index"
 
 jest.mock("next/router", () => jest.requireActual("next-router-mock"))
 
 describe("Search Results page", () => {
-  it("spaghetti", () => {
-    const query = "spaghetti"
-    mockRouter.push(`/search?q=${query}`)
-    render(<SearchResults results={results} />)
+  describe("More than 50 bibs", () => {
+    it("displays many bibs", () => {
+      const query = "spaghetti"
+      mockRouter.push(`/search?q=${query}`)
+      render(<SearchResults results={results} />)
 
-    const displayingText = screen.getByRole("heading", { level: 3 })
-    expect(displayingText).toHaveTextContent(
-      `Displaying 1-50 of ${results.results.totalResults} results for keyword "${query}"`
-    )
+      const displayingText = screen.getByRole("heading", { level: 3 })
+      expect(displayingText).toHaveTextContent(
+        `Displaying 1-50 of ${results.results.totalResults} results for keyword "${query}"`
+      )
+      const cards = screen.getAllByRole("heading", { level: 4 })
+      expect(cards).toHaveLength(50)
+    })
+  })
+  describe("No bibs", () => {
+    it("displays No results message", () => {
+      render(<SearchResults results={noBibs} />)
+
+      const noResultsMessage = screen.getByRole("heading", { level: 3 })
+      expect(noResultsMessage).toHaveTextContent(
+        "No results. Try a different search."
+      )
+      const cards = screen.queryAllByRole("heading", { level: 4 })
+      expect(cards).toHaveLength(0)
+    })
   })
 })
