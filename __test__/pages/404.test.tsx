38997fba557a8afe807f8e5546bc7a79a0fd9a05
Vera Kahn <verakahn@nypl.org>
2023-10-10 16:06:31 -0400

    finish 404 tests

--- __test__/pages/404.test.tsx
+++ __test__/pages/404.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react"
 
 import Custom404 from "../../pages/404"
 import Redirect404 from "../../pages/404/redirect"
+import { LEGACY_CATALOG_URL } from "../../src/config/constants"
 
 describe("404", () => {
   it("should display 404 text", () => {
@@ -12,11 +13,15 @@ describe("404", () => {
     const heading = screen.getByRole("heading")
     expect(heading).toHaveTextContent(FourOhFourText)
   })
-  it("should have links to circ and legacy catalogs", () => {
+  it("should have links to homepage and legacy catalogs", () => {
     render(<Custom404 />)
 
-    const links = screen.getAllByRole("link")
-    expect(links[0])
+    const homeLink = screen.getByRole("link", { name: "Research Catalog" })
+    expect(homeLink).toHaveAttribute("href", "/")
+    const legacyLink = screen.getByRole("link", {
+      name: "Legacy Catalog",
+    })
+    expect(legacyLink).toHaveAttribute("href", LEGACY_CATALOG_URL)
   })
 })
 
