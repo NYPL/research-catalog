fddd342420089fb0bb97f4f4d976892bb07176a7
Diego Cohen <diegocohen@nypl.org>
2023-10-19 16:18:53 -0400

    Initialize ItemTable model

---
+++ src/models/ItemTable.ts
@@ -0,0 +1,7 @@
+/**
+ * The ItemTable class contains the data and getter functions
+ * used to generate an Item Table component from a Bib's items.
+ *
+ * An ItemTable component appears both in Search Results and Bib pages.
+ */
+export default class ItemTable {}
