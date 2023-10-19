54b99d9d62db2bf96351c3f60d4f85fb03579987
Diego Cohen <diegocohen@nypl.org>
2023-10-19 13:32:23 -0400

    Render card heading and link href

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -1,10 +1,12 @@
 import {
   Card,
   CardContent,
-  Text,
   Link as DSLink,
+  CardHeading,
 } from "@nypl/design-system-react-components"
+
 import type SearchResultsBib from "../../models/SearchResultsBib"
+import { PATHS } from "../../config/constants"
 
 interface SearchResultProps {
   bib: SearchResultsBib
@@ -16,10 +18,11 @@ interface SearchResultProps {
 const SearchResult = ({ bib }: SearchResultProps) => {
   return (
     <Card>
+      <CardHeading level="three">
+        <DSLink href={`${PATHS.BIB}/${bib.id}`}>{bib.title}</DSLink>
+      </CardHeading>
       <CardContent>
-        <DSLink href="/">
-          <Text size="subtitle1">{bib.title}</Text>
-        </DSLink>
+        <ul></ul>
       </CardContent>
     </Card>
   )
