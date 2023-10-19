0abce5b7e4d9f0d0bddeace6a71f32258fbe542a
Diego Cohen <diegocohen@nypl.org>
2023-10-19 14:13:52 -0400

    Add space and border to Card

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -19,12 +19,20 @@ interface SearchResultProps {
  */
 const SearchResult = ({ bib }: SearchResultProps) => {
   return (
-    <Card>
+    <Card
+      sx={{
+        borderBottom: "1px solid var(--nypl-colors-ui-border-default)",
+        paddingBottom: "var(--nypl-space-m)",
+        marginBottom: "var(--nypl-space-l)",
+      }}
+    >
       <CardHeading level="three">
         <DSLink href={`${PATHS.BIB}/${bib.id}`}>{bib.title}</DSLink>
       </CardHeading>
       <CardContent>
-        <Box>
+        <Box
+          sx={{ p: { display: "inline", marginRight: "var(--nypl-space-s)" } }}
+        >
           {bib.materialType && <Text>{bib.materialType}</Text>}
           {bib.publicationStatement && <Text>{bib.publicationStatement}</Text>}
           {bib.yearPublished && <Text>{bib.yearPublished}</Text>}
