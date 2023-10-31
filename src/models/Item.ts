02e91f10e608746c1806e251cbb360398e94572b
Diego Cohen <diego.cohen3@gmail.com>
2023-10-31 15:19:44 -0400

    Rename to Item class and update comment

---
+++ src/models/Item.ts
@@ -0,0 +1,6 @@
+/**
+ * The Item class contains the data and getter functions
+ * associated with an item, as well as helper functions required
+ * for building an ItemTable.
+ */
+export default class Item {}
