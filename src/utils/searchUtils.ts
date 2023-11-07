5843aca80da49150893acac2887aa9f78be4c1b3
Diego Cohen <diegocohen@nypl.org>
2023-11-07 10:15:13 -0500

    Update mapQueryToSearchParams comment

--- src/utils/searchUtils.ts
+++ src/utils/searchUtils.ts
@@ -185,7 +185,7 @@ export function mapElementsToSearchResultsBibs(
 /**
  * mapQueryToSearchParams
  * Maps the SearchQueryParams structure from the request to a SearchParams object, which is expected by fetchResults
- * It also parses the page from a string to a number, defaulting to 1 if absent
+ * It also parses the results page number from a string, defaulting to 1 if absent
  */
 export function mapQueryToSearchParams({
   q,
