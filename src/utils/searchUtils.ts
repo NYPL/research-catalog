2ed5716bc51faa1b6f58aabeb943153be9426b3c
Diego Cohen <diego.cohen3@gmail.com>
2023-11-06 19:00:29 -0500

    Set default page number in mapQueryToSearchParams

--- src/utils/searchUtils.ts
+++ src/utils/searchUtils.ts
@@ -204,7 +204,7 @@ export function mapQueryToSearchParams({
   return {
     q,
     field: search_scope,
-    page: parseInt(page),
+    page: page ? parseInt(page) : 1,
     contributor,
     title,
     subject,
