96816e986006b39cd37f9062b76ddd2337b9bc93
Vera Kahn <verakahn@nypl.org>
2023-12-12 15:43:32 -0500

    render barcode on 404 page

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -5,6 +5,8 @@ import { appConfig } from "../../src/config/config"
 import Layout from "../../src/components/Layout/Layout"
 import RCLink from "../../src/components/RCLink/RCLink"
 
+import Barcode from "react-barcode"
+
 export default function Custom404() {
   return (
     <>
@@ -13,6 +15,7 @@ export default function Custom404() {
       </Head>
       <Layout activePage="404">
         <Heading level="h1">404 Not Found</Heading>
+        <Barcode value="23333121538324" format="codabar" />
         <p>We&apos;re sorry...</p>
         <p>The page you were looking for doesn&apos;t exist.</p>
         <p>
