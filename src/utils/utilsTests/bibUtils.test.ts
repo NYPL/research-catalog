e18b1404e528e66b561b9af259c9478320c80bf2
Diego Cohen <diego.cohen3@gmail.com>
2023-12-24 20:35:14 -0500

    Add tests for isNyplBibID

--- src/utils/utilsTests/bibUtils.test.ts
+++ src/utils/utilsTests/bibUtils.test.ts
@@ -1,36 +1,37 @@
-import { standardizeBibId } from "../bibUtils"
+import { isNyplBibID, standardizeBibId } from "../bibUtils"
 
 describe("bibUtils", () => {
   describe("standardizeBibId", () => {
-    describe("standardizeBib", () => {
-      it("doesn't mess with kosher id", () => {
-        expect(standardizeBibId("b12345678")).toBe("b12345678")
-        expect(standardizeBibId("hb123456789123456789")).toBe(
-          "hb123456789123456789"
-        )
-      })
-      it("removes check digit", () => {
-        expect(standardizeBibId("b12345678x")).toBe("b12345678")
-        expect(standardizeBibId("b12345678X")).toBe("b12345678")
-        expect(standardizeBibId("b123456781")).toBe("b12345678")
-      })
-      it("lower cases everything", () => {
-        expect(standardizeBibId("B12345678")).toBe("b12345678")
-        expect(standardizeBibId("CB1234567")).toBe("cb1234567")
-        expect(standardizeBibId("Hb123456789123456789")).toBe(
-          "hb123456789123456789"
-        )
-        expect(standardizeBibId("PB1234567")).toBe("pb1234567")
-        expect(standardizeBibId("PB1234567812345678")).toBe(
-          "pb1234567812345678"
-        )
-      })
-      it("returns value provided if input does not match bib id regexes", () => {
-        expect(standardizeBibId("b1234567899")).toBe("b1234567899")
-        expect(standardizeBibId("i am not a bib id hb123")).toBe(
-          "i am not a bib id hb123"
-        )
-      })
+    it("doesn't mess with kosher id", () => {
+      expect(standardizeBibId("b12345678")).toBe("b12345678")
+      expect(standardizeBibId("hb123456789123456789")).toBe(
+        "hb123456789123456789"
+      )
     })
+    it("removes check digit", () => {
+      expect(standardizeBibId("b12345678x")).toBe("b12345678")
+      expect(standardizeBibId("b12345678X")).toBe("b12345678")
+      expect(standardizeBibId("b123456781")).toBe("b12345678")
+    })
+    it("lower cases everything", () => {
+      expect(standardizeBibId("B12345678")).toBe("b12345678")
+      expect(standardizeBibId("CB1234567")).toBe("cb1234567")
+      expect(standardizeBibId("Hb123456789123456789")).toBe(
+        "hb123456789123456789"
+      )
+      expect(standardizeBibId("PB1234567")).toBe("pb1234567")
+      expect(standardizeBibId("PB1234567812345678")).toBe("pb1234567812345678")
+    })
+    it("returns value provided if input does not match bib id regexes", () => {
+      expect(standardizeBibId("b1234567899")).toBe("b1234567899")
+      expect(standardizeBibId("i am not a bib id hb123")).toBe(
+        "i am not a bib id hb123"
+      )
+    })
+  })
+  describe("isNyplBibID", () => {
+    expect(isNyplBibID("b12082323")).toBe(true)
+    expect(isNyplBibID("pb123456")).toBe(false)
+    expect(isNyplBibID("hb10000202040400")).toBe(false)
   })
 })
