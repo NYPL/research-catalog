3c947d87f7fc3b4da2244f9485ee815d9dd062ad
Diego Cohen <diego.cohen3@gmail.com>
2024-01-11 14:40:58 -0500

    Move tracking call to useEffect without dependency array

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import { useEffect, useRef } from "react"
+import { useEffect } from "react"
 import { useRouter } from "next/router"
 import "@nypl/design-system-react-components/dist/styles.css"
 import { trackVirtualPageView } from "../src/utils/appUtils"
@@ -7,32 +7,11 @@ import { trackVirtualPageView } from "../src/utils/appUtils"
 // eslint-disable-next-line @typescript-eslint/naming-convention
 function App({ Component, pageProps }) {
   const router = useRouter()
-  // Prevents double-firing of useEffect on initial page load
-  const initialized = useRef(false)
 
-  // Track initial page view to Adobe Analytics
+  // Track page view events to Adobe Analytics
   useEffect(() => {
-    if (!initialized.current) {
-      initialized.current = true
-      trackVirtualPageView(router.asPath)
-    }
-  }, [router.asPath])
-
-  // Track subsequent page views to Adobe Analytics
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      trackVirtualPageView(url)
-    }
-    // When the component is mounted, subscribe to router changes
-    // and track those page views
-    router.events.on("routeChangeComplete", handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the "off" method
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange)
-    }
-  }, [router.events])
+    trackVirtualPageView(router.asPath)
+  })
 
   return (
     <>
