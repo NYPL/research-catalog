51dfc1f3b0a7d5c7ec9754c7d8abeb7ab21f8ca7
Diego Cohen <diego.cohen3@gmail.com>
2024-01-22 12:37:32 -0500

    Keep react scriptmode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   basePath: "/research/research-catalog",
   // pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 }
