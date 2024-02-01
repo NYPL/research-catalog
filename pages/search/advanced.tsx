838f04f81685f2898c41ebe0b54977beb3439340
Diego Cohen <diegocohen@nypl.org>
2024-02-01 11:11:09 -0500

    Add optional chaining to feature flag includes statement

--- pages/search/advanced.tsx
+++ pages/search/advanced.tsx
@@ -100,7 +100,7 @@ export default function AdvancedSearch() {
     } else {
       // If the reverse_proxy_enabled feature flag is present, use window.location.replace
       // instead of router.push to forward search results to DFE.
-      if (process.env.NEXT_PUBLIC_FEATURES.includes("reverse_proxy_enabled")) {
+      if (process.env.NEXT_PUBLIC_FEATURES?.includes("reverse_proxy_enabled")) {
         window.location.replace(`${BASE_URL}${PATHS.SEARCH}${queryString}`)
       } else {
         await router.push(`${PATHS.SEARCH}${queryString}`)
