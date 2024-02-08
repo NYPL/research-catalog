160719f892aa3e117bd27472bff083f85fc361b5
Vera Kahn <verakahn@nypl.org>
2024-02-08 10:17:07 -0500

    linting

--- src/models/modelTests/data/MyAccount.js
+++ src/models/modelTests/data/MyAccount.js
@@ -3,7 +3,8 @@ export const holds = {
   start: 0,
   entries: [
     {
-      "patron": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
+      patron:
+        "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
       id: "https://ilsstaff.nypl.org/iii/sierra-api/v6/patrons/holds/48636910",
       record: "https://ilsstaff.nypl.org/iii/sierra-api/v6/items/37603650",
       frozen: false,
@@ -26,42 +27,46 @@ export const holds = {
 }
 
 export const checkouts = {
-  "total": 4,
-  "start": 0,
-  "entries": [
+  total: 4,
+  start: 0,
+  entries: [
     {
-      "id": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/checkouts/58536119",
-      "patron": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
-      "item": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/items/39481193",
-      "barcode": "32101043146511",
-      "dueDate": "2023-09-27T08:00:00Z",
-      "callNumber": "HD9890.M88"
+      id: "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/checkouts/58536119",
+      patron:
+        "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
+      item: "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/items/39481193",
+      barcode: "32101043146511",
+      dueDate: "2023-09-27T08:00:00Z",
+      callNumber: "HD9890.M88",
     },
     {
-      "id": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/checkouts/58536118",
-      "patron": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
-      "item": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/items/39481194",
-      "barcode": "32101088255243",
-      "dueDate": "2023-09-27T08:00:00Z",
-      "callNumber": "HE396.E4 H7"
+      id: "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/checkouts/58536118",
+      patron:
+        "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
+      item: "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/items/39481194",
+      barcode: "32101088255243",
+      dueDate: "2023-09-27T08:00:00Z",
+      callNumber: "HE396.E4 H7",
     },
     {
-      "id": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/checkouts/58536117",
-      "patron": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
-      "item": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/items/39481179",
-      "barcode": "CU00569410",
-      "dueDate": "2023-09-27T08:00:00Z",
-      "callNumber": "PL2627 .C472 1987 v.30-34"
+      id: "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/checkouts/58536117",
+      patron:
+        "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
+      item: "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/items/39481179",
+      barcode: "CU00569410",
+      dueDate: "2023-09-27T08:00:00Z",
+      callNumber: "PL2627 .C472 1987 v.30-34",
     },
     {
-      "id": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/checkouts/58536116",
-      "patron": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
-      "item": "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/items/39481192",
-      "barcode": "CU73981141",
-      "dueDate": "2023-09-27T08:00:00Z",
-      "callNumber": "PR6053.R378 T53 2008g"
-    }
-  ]
+      id: "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/checkouts/58536116",
+      patron:
+        "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/patrons/2772226",
+      item: "https://nypl-sierra-test.nypl.org/iii/sierra-api/v6/items/39481192",
+      barcode: "CU73981141",
+      dueDate: "2023-09-27T08:00:00Z",
+      callNumber: "PR6053.R378 T53 2008g",
+    },
+  ],
 }
 
 export const patron = {
