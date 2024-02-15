c487f3c805097016bc9894f4f4a28bf1dc47cb78
Vera Kahn <verakahn@nypl.org>
2024-02-15 17:54:32 -0500

    extract kms decrypt

--- src/server/nyplApiClient/index.ts
+++ src/server/nyplApiClient/index.ts
@@ -2,42 +2,19 @@ import NyplApiClient from "@nypl/nypl-data-api-client"
 import aws from "aws-sdk"
 
 import { appConfig } from "../../config/config"
+import { kmsDecryptCreds } from "../kms"
 
 interface KMSCache {
   clients: string[]
-  clientId: string
-  clientSecret: string
+  id: string
+  secret: string
 }
 const appEnvironment = process.env.APP_ENV || "production"
-const clientId = process.env.PLATFORM_API_CLIENT_ID
-const clientSecret = process.env.PLATFORM_API_CLIENT_SECRET
+const encryptedClientId = process.env.PLATFORM_API_CLIENT_ID
+const encryptedClientSecret = process.env.PLATFORM_API_CLIENT_SECRET
 
-const keys = [clientId, clientSecret]
-const CACHE: KMSCache = { clients: [], clientSecret, clientId }
-
-const kms: aws.KMS = new aws.KMS({
-  region: "us-east-1",
-})
-
-const decryptKMS = async (key: string) => {
-  console.log("Decrypt func INPUT: " + key)
-  const params = {
-    CiphertextBlob: Buffer.from(key, "base64"),
-  }
-  const { Plaintext } = await kms.decrypt(params).promise()
-
-  return await new Promise((resolve, reject) => {
-    kms.decrypt(params, (err: Error) => {
-      if (err) {
-        console.log({ err })
-        reject(err)
-      } else {
-        // eslint-disable-next-line @typescript-eslint/no-base-to-string
-        resolve(Plaintext.toString())
-      }
-    })
-  })
-}
+const creds = [encryptedClientId, encryptedClientSecret]
+const CACHE: KMSCache = { clients: [], secret: null, id: null }
 
 const nyplApiClient = async (options = { apiName: "platform" }) => {
   const { apiName } = options
@@ -46,28 +23,33 @@ const nyplApiClient = async (options = { apiName: "platform" }) => {
   }
 
   const baseUrl = appConfig.apiUrls[apiName][appEnvironment]
-  return await new Promise((resolve, reject) => {
-    Promise.all(keys.map(decryptKMS))
-      .then(([decryptedClientId, decryptedClientSecret]) => {
-        const nyplApiClient = new NyplApiClient({
-          base_url: baseUrl,
-          oauth_key: decryptedClientId,
-          oauth_secret: decryptedClientSecret,
-          oauth_url: appConfig.tokenUrl,
-        })
 
-        CACHE.clientId = clientId
-        CACHE.clientSecret = clientSecret
-        CACHE.clients[apiName] = nyplApiClient
-
-        resolve(nyplApiClient)
-      })
-      .catch((error) => {
-        // TODO: add this once the Winston config is added.
-        // logger.error('ERROR trying to decrypt using KMS.', error);
-        reject(error)
-      })
-  })
+  let decryptedId: string
+  let decryptedSecret: string
+  if (CACHE.secret && CACHE.secret) {
+    decryptedId = CACHE.id
+    decryptedSecret = CACHE.secret
+  } else {
+    try {
+      ;[decryptedId, decryptedSecret] = await kmsDecryptCreds(creds)
+      CACHE.id = decryptedId
+      CACHE.secret = decryptedSecret
+    } catch (exception) {
+      console.error("Error decrypting Sierra credentials")
+    }
+  }
+  try {
+    const nyplApiClient = new NyplApiClient({
+      base_url: baseUrl,
+      oauth_key: decryptedId,
+      oauth_secret: decryptedSecret,
+      oauth_url: appConfig.tokenUrl,
+    })
+    CACHE.clients[apiName] = nyplApiClient
+    return nyplApiClient
+  } catch (error) {
+    console.error(error.message)
+  }
 }
 
 export default nyplApiClient
