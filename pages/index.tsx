83d15b2155281b38e2135e398c540156df646f15
Diego Cohen <diego.cohen3@gmail.com>
2024-02-16 15:17:41 -0500

    Add console logs

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,6 +27,7 @@ export default function Home({
   bannerNotification,
   isAuthenticated,
 }: HomeProps) {
+  console.log("appConfig.features", appConfig.features)
   const isLoading = useLoading()
   return (
     <>
@@ -192,7 +193,7 @@ export async function getServerSideProps({ req }) {
   // Now it can be used to get patron data from Sierra or Platform API
   // or use `isTokenValid` to redirect to login page if it's not valid.
   console.log("patronTokenResponse is", patronTokenResponse)
-  console.log("appConfig.features", appConfig.features)
+  console.log("process.env.APP_ENV", process.env.APP_ENV)
   const isAuthenticated = patronTokenResponse.isTokenValid
   // return props object
   return {
