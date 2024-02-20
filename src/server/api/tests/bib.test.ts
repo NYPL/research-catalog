2ad5da80d1956d9a0640942bba01ff626814290b
Diego Cohen <diego.cohen3@gmail.com>
2024-02-20 15:46:39 -0500

    Add test for errors

--- src/server/api/tests/bib.test.ts
+++ src/server/api/tests/bib.test.ts
@@ -10,61 +10,16 @@ jest.mock("../../nyplApiClient", () => {
           get: jest
             .fn()
             .mockResolvedValueOnce({
-              creatorLiteral: ["De Regniers, Beatrice Schenk."],
-              dateStartYear: 1958,
-              dimensions: ["31 cm."],
-              electronicResources: [],
-              extent: ["1 v. (unpaged) illus."],
-              identifier: [
-                { "@type": "nypl:Bnumber", "@value": "17418167" },
-                { "@type": "nypl:Oclc", "@value": "1662173" },
-                { "@type": "bf:Lccn", "@value": "57013360" },
-              ],
-              issuance: [
-                { "@id": "urn:biblevel:m", prefLabel: "monograph/item" },
-              ],
-              items: [
-                {
-                  "@id": "res:i22253043",
-                  "@type": [Array],
-                  accessMessage: [Array],
-                  catalogItemType: [Array],
-                  eddRequestable: true,
-                  formatLiteral: [Array],
-                  holdingLocation: [Array],
-                  idBarcode: [Array],
-                  identifier: [Array],
-                  m2CustomerCode: [Array],
-                  physRequestable: true,
-                  physicalLocation: [Array],
-                  requestable: [Array],
-                  shelfMark: [Array],
-                  specRequestable: false,
-                  status: [Array],
-                  uri: "i22253043",
-                  idNyplSourceId: [Object],
-                },
-              ],
+              items: [{}],
               numItemsTotal: 1,
               title: ["Cats cats cats cats cats."],
-              titleDisplay: [
-                "Cats cats cats cats cats. Drawing and design by Bill Sokol.",
-              ],
               uri: "b17418167",
             })
             .mockResolvedValueOnce({
               bib: {
                 id: "17418167",
                 nyplSource: "sierra-nypl",
-                fields: [
-                  { label: "Author", values: [[]] },
-                  { label: "Title", values: [[]] },
-                  { label: "Imprint", values: [[]] },
-                  { label: "Edition", values: [[]] },
-                  { label: "Description", values: [[]] },
-                  { label: "LCCN", values: [[]] },
-                  { label: "Branch Call Number", values: [[]] },
-                ],
+                fields: [{}, {}, {}, {}, {}, {}, {}],
               },
             }),
         })
@@ -76,41 +31,7 @@ jest.mock("../../nyplApiClient", () => {
           get: jest
             .fn()
             .mockResolvedValueOnce({
-              creatorLiteral: ["De Regniers, Beatrice Schenk."],
-              dateStartYear: 1958,
-              dimensions: ["31 cm."],
-              electronicResources: [],
-              extent: ["1 v. (unpaged) illus."],
-              identifier: [
-                { "@type": "nypl:Bnumber", "@value": "17418167" },
-                { "@type": "nypl:Oclc", "@value": "1662173" },
-                { "@type": "bf:Lccn", "@value": "57013360" },
-              ],
-              issuance: [
-                { "@id": "urn:biblevel:m", prefLabel: "monograph/item" },
-              ],
-              items: [
-                {
-                  "@id": "res:i22253043",
-                  "@type": [Array],
-                  accessMessage: [Array],
-                  catalogItemType: [Array],
-                  eddRequestable: true,
-                  formatLiteral: [Array],
-                  holdingLocation: [Array],
-                  idBarcode: [Array],
-                  identifier: [Array],
-                  m2CustomerCode: [Array],
-                  physRequestable: true,
-                  physicalLocation: [Array],
-                  requestable: [Array],
-                  shelfMark: [Array],
-                  specRequestable: false,
-                  status: [Array],
-                  uri: "i22253043",
-                  idNyplSourceId: [Object],
-                },
-              ],
+              items: [{}],
               numItemsTotal: 1,
               title: ["Cats cats cats cats cats."],
               titleDisplay: [
@@ -121,15 +42,7 @@ jest.mock("../../nyplApiClient", () => {
               bib: {
                 id: "17418167",
                 nyplSource: "sierra-nypl",
-                fields: [
-                  { label: "Author", values: [[]] },
-                  { label: "Title", values: [[]] },
-                  { label: "Imprint", values: [[]] },
-                  { label: "Edition", values: [[]] },
-                  { label: "Description", values: [[]] },
-                  { label: "LCCN", values: [[]] },
-                  { label: "Branch Call Number", values: [[]] },
-                ],
+                fields: [{}, {}, {}, {}, {}, {}, {}],
               },
             })
             .mockResolvedValueOnce({
@@ -138,11 +51,43 @@ jest.mock("../../nyplApiClient", () => {
         })
       })
     })
+    .mockImplementationOnce(async () => {
+      return await new Promise((resolve) => {
+        resolve({
+          get: jest
+            .fn()
+            .mockResolvedValueOnce({
+              items: [{}],
+              numItemsTotal: 1,
+              title: ["Cats cats cats cats cats."],
+            })
+            .mockResolvedValueOnce({
+              bib: {
+                id: "17418167",
+                nyplSource: "sierra-nypl",
+                fields: [{}, {}, {}, {}, {}, {}, {}],
+              },
+            })
+            .mockResolvedValueOnce({
+              statusCode: 404,
+            }),
+        })
+      })
+    })
+    .mockImplementationOnce(async () => {
+      return await new Promise((resolve) => {
+        resolve({
+          get: () => {
+            throw new Error("Bad API URL")
+          },
+        })
+      })
+    })
 })
 
 describe("fetchBib", () => {
   it("should return bib and annotated data with 200 status code when uri is present", async () => {
-    const bibResponse = (await fetchBib("b17418167", {})) as BibResponse
+    const bibResponse = (await fetchBib("b17418167")) as BibResponse
     expect(bibResponse.bib.numItemsTotal).toEqual(1)
     expect(bibResponse.bib.title).toEqual(["Cats cats cats cats cats."])
 
@@ -153,7 +98,7 @@ describe("fetchBib", () => {
   })
 
   it("should redirect when uri is not present and sierra call responds with 200 status", async () => {
-    const bibResponse = (await fetchBib("b17418167", {})) as BibResponse
+    const bibResponse = (await fetchBib("b17418167")) as BibResponse
 
     expect(bibResponse.status).toEqual(307)
     expect(bibResponse.redirectUrl).toEqual(
@@ -161,10 +106,16 @@ describe("fetchBib", () => {
     )
   })
 
+  it("should respond with a 404 status when uri is not present and sierra call responds with a non-200 status", async () => {
+    const bibResponse = (await fetchBib("b17418167")) as BibResponse
+
+    expect(bibResponse.status).toEqual(404)
+  })
+
   // Intentionally throw an error from the NYPLApiClient
-  // it("should throw an error if there was one", async () => {
-  //   await expect(
-  //     async () => (await fetchResults({ q: "cat" })) as SearchResultsResponse
-  //   ).rejects.toThrow("Bad API URL")
-  // })
+  it("should throw an error if there was one", async () => {
+    await expect(
+      async () => (await fetchBib("b17418167")) as BibResponse
+    ).rejects.toThrow("Bad API URL")
+  })
 })
