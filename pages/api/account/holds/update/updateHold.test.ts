32adfe10c1dfa25d4516588d1bc26ecff44d7629
Vera Kahn <verakahn@nypl.org>
2024-02-27 12:02:12 -0500

    mock hold update;

--- pages/api/account/holds/update/updateHold.test.ts
+++ pages/api/account/holds/update/updateHold.test.ts
@@ -6,7 +6,7 @@ import type { NextApiRequest, NextApiResponse } from "next"
 jest.mock("../../../../../src/server/sierraClient")
 jest.mock("../../../../../src/server/auth")
 
-const mockHoldUpdate = (holdUpdateReturnValue) => {
+const mockHoldUpdate = (holdUpdateReturnValue = "") => {
   const mockedHoldUpdate = jest.fn(async () => holdUpdateReturnValue)
   jest.mock("./[id]", () => {
     const handler = jest.requireActual("./[id]")
@@ -18,8 +18,9 @@ const mockHoldUpdate = (holdUpdateReturnValue) => {
 describe("handler", () => {
   let req: Partial<NextApiRequest>
   let res: Partial<NextApiResponse>
-
+  let mockedHoldUpdate: jest.Mock
   beforeEach(() => {
+    mockedHoldUpdate = mockHoldUpdate()
     req = {
       cookies: {},
       method: "POST",
@@ -37,7 +38,6 @@ describe("handler", () => {
     ;(initializePatronTokenAuth as jest.Mock).mockResolvedValueOnce({
       decodedPatron: null,
     })
-    const mockedHoldUpdate = mockHoldUpdate("")
     await handler(req as NextApiRequest, res as NextApiResponse)
     expect(mockedHoldUpdate).not.toHaveBeenCalled()
     expect(res.status).toHaveBeenCalledWith(403)
@@ -51,7 +51,7 @@ describe("handler", () => {
     })
 
     await handler(req as NextApiRequest, res as NextApiResponse)
-    expect(holdUpdate).not.toHaveBeenCalled()
+    expect(mockedHoldUpdate).not.toHaveBeenCalled()
     expect(res.status).toHaveBeenCalledWith(403)
     expect(res.json).toHaveBeenCalledWith(
       "Authenticated patron does not own this hold"
@@ -63,7 +63,7 @@ describe("handler", () => {
       decodedPatron: { sub: "123456" },
     })
     await handler(req as NextApiRequest, res as NextApiResponse)
-    expect(holdUpdate).toHaveBeenCalled
+    expect(mockedHoldUpdate).toHaveBeenCalled
   })
 })
 
