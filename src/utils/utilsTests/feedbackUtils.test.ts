a2cc0eba2507f1b6efba79c61fa8ff8c963b088d
Diego Cohen <diegocohen@nypl.org>
2024-02-27 16:13:40 -0500

    Fix nypl url

--- src/utils/utilsTests/feedbackUtils.test.ts
+++ src/utils/utilsTests/feedbackUtils.test.ts
@@ -4,7 +4,7 @@ describe("feedbackUtils", () => {
   describe("getFeedbackEmailText", () => {
     it("correctly format the feedback email text given a full url and a fields object", () => {
       expect(
-        getFeedbackEmailText("https://www,nypl.org", {
+        getFeedbackEmailText("https://www.nypl.org", {
           category: "category",
           comment: "comment",
           id: "b12345678",
@@ -12,24 +12,24 @@ describe("feedbackUtils", () => {
           barcode: "12345678",
         })
       ).toBe(
-        "Question/Feedback from Research Catalog (SCC): category: category, comment: comment, id: b12345678, email: test@nypl.org, barcode: 12345678 URL: https://www,nypl.org"
+        "Question/Feedback from Research Catalog (SCC): category: category, comment: comment, id: b12345678, email: test@nypl.org, barcode: 12345678 URL: https://www.nypl.org"
       )
     })
     it("correctly format the feedback email text given a full url and a fields object with just a comment and email", () => {
       expect(
-        getFeedbackEmailText("https://www,nypl.org", {
+        getFeedbackEmailText("https://www.nypl.org", {
           comment: "comment",
           email: "test@nypl.org",
         })
       ).toBe(
-        "Question/Feedback from Research Catalog (SCC): comment: comment, email: test@nypl.org URL: https://www,nypl.org"
+        "Question/Feedback from Research Catalog (SCC): comment: comment, email: test@nypl.org URL: https://www.nypl.org"
       )
     })
   })
   describe("getFeedbackEmailHTML", () => {
     it("correctly format the feedback email HTML given a full url and a fields object", () => {
       expect(
-        getFeedbackEmailHTML("https://www,nypl.org", {
+        getFeedbackEmailHTML("https://www.nypl.org", {
           category: "category",
           comment: "comment",
           id: "b12345678",
@@ -58,7 +58,7 @@ describe("feedbackUtils", () => {
             <dd>12345678</dd>
           
           <dt>URL:</dt>
-          <dd>https://www,nypl.org</dd>
+          <dd>https://www.nypl.org</dd>
         </dl>
       </div>
     `
