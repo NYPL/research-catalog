b0aeb8ca8ec3da94f74b2befa4af8826fd6754a7
Diego Cohen <diegocohen@nypl.org>
2024-02-27 16:13:05 -0500

    Add test for getFeedbackEmailHTML rendering

--- src/utils/utilsTests/feedbackUtils.test.ts
+++ src/utils/utilsTests/feedbackUtils.test.ts
@@ -1,8 +1,8 @@
-import { getFeedbackEmailText } from "../feedbackUtils"
+import { getFeedbackEmailText, getFeedbackEmailHTML } from "../feedbackUtils"
 
 describe("feedbackUtils", () => {
   describe("getFeedbackEmailText", () => {
-    it("correctly format the feedback email test given a full url and a fields object", () => {
+    it("correctly format the feedback email text given a full url and a fields object", () => {
       expect(
         getFeedbackEmailText("https://www,nypl.org", {
           category: "category",
@@ -15,7 +15,7 @@ describe("feedbackUtils", () => {
         "Question/Feedback from Research Catalog (SCC): category: category, comment: comment, id: b12345678, email: test@nypl.org, barcode: 12345678 URL: https://www,nypl.org"
       )
     })
-    it("correctly format the feedback email test given a full url and a fields object with just a comment and email", () => {
+    it("correctly format the feedback email text given a full url and a fields object with just a comment and email", () => {
       expect(
         getFeedbackEmailText("https://www,nypl.org", {
           comment: "comment",
@@ -23,6 +23,45 @@ describe("feedbackUtils", () => {
         })
       ).toBe(
         "Question/Feedback from Research Catalog (SCC): comment: comment, email: test@nypl.org URL: https://www,nypl.org"
+      )
+    })
+  })
+  describe("getFeedbackEmailHTML", () => {
+    it("correctly format the feedback email HTML given a full url and a fields object", () => {
+      expect(
+        getFeedbackEmailHTML("https://www,nypl.org", {
+          category: "category",
+          comment: "comment",
+          id: "b12345678",
+          email: "test@nypl.org",
+          barcode: "12345678",
+        })
+      ).toBe(
+        `
+      <div>
+        <h1>Question/Feedback from Research Catalog (SCC):</h1>
+        <dl>
+          
+            <dt>category:</dt>
+            <dd>category</dd>
+          
+            <dt>comment:</dt>
+            <dd>comment</dd>
+          
+            <dt>id:</dt>
+            <dd>b12345678</dd>
+          
+            <dt>email:</dt>
+            <dd>test@nypl.org</dd>
+          
+            <dt>barcode:</dt>
+            <dd>12345678</dd>
+          
+          <dt>URL:</dt>
+          <dd>https://www,nypl.org</dd>
+        </dl>
+      </div>
+    `
       )
     })
   })
