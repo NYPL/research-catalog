c1558d3e109e964fd711abf7dbd8d08f7846fbad
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-03-01 14:08:39 -0500

    Adding mock router to account index page test

--- __test__/pages/account/account.test.tsx
+++ __test__/pages/account/account.test.tsx
@@ -1,7 +1,20 @@
 import MyAccount from "../../../pages/account/[[...index]]"
 import { render, screen } from "@testing-library/react"
+import { useRouter } from "next/router"
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}))
 
 describe("My Account page", () => {
+  const routerMock = useRouter as jest.Mock
+  routerMock.mockReturnValue({
+    push: jest.fn(),
+    events: {
+      on: jest.fn(),
+      off: jest.fn(),
+    },
+  })
   it("displays an error message when patron is empty", () => {
     render(<MyAccount isAuthenticated={true} />)
     expect(screen.getByText("We are unable to display", { exact: false }))
