902e520e03dd82b45f3c21fc14fa551befae1ddb
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-03-04 12:30:23 -0500

    Stubbing tests for upcoming redirects ticket

--- __test__/pages/account/account.test.tsx
+++ __test__/pages/account/account.test.tsx
@@ -15,4 +15,9 @@ describe("My Account page", () => {
     render(<MyAccount isAuthenticated={true} />)
     expect(screen.getByText("We are unable to display", { exact: false }))
   })
+
+  it.todo("redirects /overdues to /account if user has no fees")
+  it.todo(
+    "redirects anything besides checkouts, overdues, requests, settings to /account if user has no fees"
+  )
 })
