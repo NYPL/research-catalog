7119a71ccffb33c1635eb7137294344a9bbe18db
Vera Kahn <verakahn@nypl.org>
2024-03-22 17:40:24 -0400

    add buildTagSetData test

--- src/components/SearchFilters/appliedFilterUtils.test.ts
+++ src/components/SearchFilters/appliedFilterUtils.test.ts
@@ -1,4 +1,7 @@
-import { buildAppliedFiltersValueArrayWithTagRemoved } from "./appliedFilterUtils"
+import {
+  buildAppliedFiltersValueArrayWithTagRemoved,
+  buildTagsetData,
+} from "./appliedFilterUtils"
 
 describe("Applied Filter utils", () => {
   describe("buildAppliedFiltersValueArrayWithTagRemoved", () => {
@@ -24,6 +27,58 @@ describe("Applied Filter utils", () => {
           appliedFiltersWithLabels
         )
       ).toStrictEqual({ tags: ["keep"] })
+    })
+  })
+  describe("buildTagsetData", () => {
+    it("returns an array of tagset data", () => {
+      const appliedFiltersWithLabels = {
+        language: [
+          {
+            value: "lang:eng",
+            count: 14,
+            label: "English",
+          },
+          {
+            value: "lang:fre",
+            count: 2,
+            label: "French",
+          },
+        ],
+        subjectLiteral: [
+          {
+            value: "Spaghetti Westerns -- History and criticism.",
+            count: 12,
+            label: "Spaghetti Westerns -- History and criticism.",
+          },
+          {
+            value: "COOKING -- General.",
+            count: 4,
+            label: "COOKING -- General.",
+          },
+        ],
+      }
+      expect(buildTagsetData(appliedFiltersWithLabels)).toStrictEqual([
+        {
+          id: "language-English",
+          label: "English",
+          field: "language",
+        },
+        {
+          id: "language-French",
+          label: "French",
+          field: "language",
+        },
+        {
+          id: "subjectLiteral-Spaghetti Westerns -- History and criticism.",
+          label: "Spaghetti Westerns -- History and criticism.",
+          field: "subjectLiteral",
+        },
+        {
+          id: "subjectLiteral-COOKING -- General.",
+          label: "COOKING -- General.",
+          field: "subjectLiteral",
+        },
+      ])
     })
   })
 })
