d6f292324785394247d3c88535776df2bc33ab5f
Vera Kahn <verakahn@nypl.org>
2024-03-25 14:14:53 -0400

    update tests

--- src/hooks/useDateForm.test.tsx
+++ src/hooks/useDateForm.test.tsx
@@ -1,6 +1,84 @@
-import { endDateInvalid, rangeContainsInvalidYearFormat } from "./useDateForm"
+import {
+  endDateInvalid,
+  rangeContainsInvalidYearFormat,
+  useDateForm,
+} from "./useDateForm"
+import { renderHook, screen, render, waitFor } from "../utils/testUtils"
+import type { MutableRefObject } from "react"
+import { useRef } from "react"
+import type { TextInputRefType } from "@nypl/design-system-react-components"
+import DateForm from "../components/SearchFilters/DateForm"
+
+const setUpDateFormWithRefs = (start, end) => {
+  let refs: MutableRefObject<TextInputRefType>[]
+  // this component exists only to populate the ref values
+  const TestComponent = () => {
+    refs = [useRef<TextInputRefType>()]
+    return null
+  }
+  render(<TestComponent />)
+  const {
+    rerender,
+    result: {
+      current: { validateDateRange, dateFormProps },
+    },
+  } = renderHook(() =>
+    useDateForm({
+      inputRefs: refs,
+      dateAfter: start,
+      dateBefore: end,
+      changeHandler: () => true,
+    })
+  )
+  return { validateDateRange, dateFormProps, rerender }
+}
 
 describe("useDateForm", () => {
+  describe("validateDateRange", () => {
+    it("validateDateRange focuses the input when end date is earlier than start date", () => {
+      const { validateDateRange, dateFormProps } = setUpDateFormWithRefs(
+        "2000",
+        "1999"
+      )
+      render(<DateForm {...dateFormProps} />)
+      validateDateRange()
+      const input1 = screen.getByDisplayValue("2000")
+      expect(input1).toHaveFocus()
+    })
+    it("validateDateRange returns false when end date is earlier than start date", async () => {
+      const { validateDateRange, dateFormProps } = setUpDateFormWithRefs(
+        "2000",
+        "1999"
+      )
+      render(<DateForm {...dateFormProps} />)
+      expect(validateDateRange()).toBe(false)
+    })
+    it(
+      "validateDateRange displays range error message when end date is earlier than start date"
+      // , () => {
+      //   const { validateDateRange, dateFormProps } = setUpDateFormWithRefs(
+      //     "2000",
+      //     "1999"
+      //   )
+      //   // dateRangeError is populated, but displayDateRangeError is ''
+      //   render(<DateForm {...dateFormProps} />)
+      //   // invocation should update display to have content
+      //   validateDateRange()
+      //   const error = screen.findByText(
+      //     "Error: Start date must be earlier than end date."
+      //   )
+      //   expect(error).toBeInTheDocument()
+      // }
+    )
+    it("returns true when dates are valid", () => {
+      const { validateDateRange, dateFormProps } = setUpDateFormWithRefs(
+        "1999",
+        "2000"
+      )
+      render(<DateForm {...dateFormProps} />)
+      expect(validateDateRange()).toBe(true)
+    })
+  })
   describe("endDateInvalid", () => {
     it("returns false if either dateBefore or dateAfter are empty", () => {
       expect(endDateInvalid("", "2000")).toBe(false)
