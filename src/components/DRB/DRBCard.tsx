a69fe55f4642e5bc1a210047aebdb50ca57085fb
Diego Cohen <diego.cohen3@gmail.com>
2024-04-08 13:21:09 -0400

    Make DRB Result titles actual titles

--- src/components/DRB/DRBCard.tsx
+++ src/components/DRB/DRBCard.tsx
@@ -4,6 +4,7 @@ import {
   Text,
   Icon,
   Link as DSLink,
+  CardHeading,
 } from "@nypl/design-system-react-components"
 
 import type DRBResult from "../../models/DRBResult"
@@ -23,17 +24,19 @@ const DRBCard = ({ drbResult }: DRBCardProps) => {
   return (
     <Card backgroundColor="ui.bg.default" p="xs">
       <CardContent>
-        <DSLink
-          href={drbResult.url}
-          target="_blank"
-          isUnderlined={false}
-          fontSize="desktop.body.body2"
-          display="inline-block"
-          mb="s"
-          lang={drbResult.language}
-        >
-          {drbResult.title}
-        </DSLink>
+        <CardHeading level="h3" size="heading6" mb="0">
+          <DSLink
+            href={drbResult.url}
+            target="_blank"
+            isUnderlined={false}
+            fontSize="desktop.body.body2"
+            display="inline-block"
+            mb="s"
+            lang={drbResult.language}
+          >
+            {drbResult.title}
+          </DSLink>
+        </CardHeading>
 
         {drbResult?.authors.length > 0 ? (
           <Text size="body2">
