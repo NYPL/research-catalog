54b90f3c8fe01dc4a26eb440ca459114159a4c40
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-04-09 16:52:54 -0400

    Redirect tests

--- __test__/pages/account/account.test.tsx
+++ __test__/pages/account/account.test.tsx
@@ -1,23 +1,100 @@
-import MyAccount from "../../../pages/account/[[...index]]"
+import MyAccount, {
+  getServerSideProps,
+} from "../../../pages/account/[[...index]]"
+import { MyAccountFactory } from "../../../src/models/MyAccount"
 import { render, screen } from "../../../src/utils/testUtils"
-import { useRouter } from "next/router"
+import initializePatronTokenAuth from "../../../src/server/auth"
+import {
+  mockCheckouts,
+  mockFines,
+  mockHolds,
+  mockPatron,
+} from "../../fixtures/myAccountFixtures"
 
-jest.mock("next/router", () => ({
-  useRouter: jest.fn(),
-}))
+jest.mock("../../../src/server/auth")
+jest.mock("../../../src/models/MyAccount")
 
-describe("My Account page", () => {
-  const routerMock = useRouter as jest.Mock
-  routerMock.mockReturnValue({
-    push: jest.fn(),
-  })
+describe("MyAccount page", () => {
   it("displays an error message when patron is empty", () => {
     render(<MyAccount isAuthenticated={true} />)
     expect(screen.getByText("We are unable to display", { exact: false }))
   })
 
-  it.todo("redirects /overdues to /account if user has no fees")
-  it.todo(
-    "redirects anything besides checkouts, overdues, requests, settings to /account if user has no fees"
-  )
+  beforeEach(() => {
+    ;(initializePatronTokenAuth as jest.Mock).mockResolvedValueOnce({
+      isTokenValid: true,
+      errorCode: null,
+      decodedPatron: { sub: "123" },
+    })
+  })
+
+  it("redirects /overdues to /account if user has no fees", async () => {
+    ;(MyAccountFactory as jest.Mock).mockResolvedValueOnce({
+      checkouts: mockCheckouts,
+      patron: mockPatron,
+      fines: { total: 0, entries: [] },
+      holds: mockHolds,
+    })
+
+    const mockReq = {
+      headers: {
+        host: "local.nypl.org:8080",
+      },
+      url: "/account/overdues",
+      cookies: {
+        nyplIdentityPatron: '{"access_token":123}',
+      },
+    }
+
+    const result = await getServerSideProps({ req: mockReq })
+    expect(result).toStrictEqual({
+      redirect: { destination: "/account", permanent: false },
+    })
+  })
+
+  it("redirects invalid paths to /account", async () => {
+    ;(MyAccountFactory as jest.Mock).mockResolvedValueOnce({
+      checkouts: mockCheckouts,
+      patron: mockPatron,
+      fines: mockFines,
+      holds: mockHolds,
+    })
+
+    const mockReq = {
+      headers: {
+        host: "local.nypl.org:8080",
+      },
+      url: "/account/spaghetti",
+      cookies: {
+        nyplIdentityPatron: '{"access_token":123}',
+      },
+    }
+
+    const result = await getServerSideProps({ req: mockReq })
+    expect(result).toStrictEqual({
+      redirect: { destination: "/account", permanent: false },
+    })
+  })
+
+  it("allows valid path to /account/settings", async () => {
+    ;(MyAccountFactory as jest.Mock).mockResolvedValueOnce({
+      checkouts: mockCheckouts,
+      patron: mockPatron,
+      fines: mockFines,
+      holds: mockHolds,
+    })
+
+    const mockReq = {
+      headers: {
+        host: "local.nypl.org:8080",
+      },
+      url: "/account/settings",
+      cookies: {
+        nyplIdentityPatron: '{"access_token":123}',
+      },
+    }
+
+    const result = await getServerSideProps({ req: mockReq })
+    expect(result.props.tabsPath).toBe("settings")
+  })
 })
