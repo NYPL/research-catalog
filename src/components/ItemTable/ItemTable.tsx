33fb4d417866066d66b5d57b77ce3a07c72af701
Edwin Guzman <edwin.gzmn@gmail.com>
2024-04-12 10:25:53 -0400

    Moving styles to module css file

--- src/components/ItemTable/ItemTable.tsx
+++ src/components/ItemTable/ItemTable.tsx
@@ -3,6 +3,7 @@ import { Box, Table } from "@nypl/design-system-react-components"
 import type ItemTableData from "../../models/ItemTableData"
 import RequestButtons from "./RequestButtons"
 import ItemAvailability from "./ItemAvailability"
+import styles from "../../../styles/components/ItemTable.module.scss"
 
 interface ItemTableProps {
   itemTableData: ItemTableData
@@ -15,24 +16,9 @@ const ItemTable = ({ itemTableData }: ItemTableProps) => {
   return (
     <>
       <Table
+        className={styles.itemTable}
         columnHeaders={itemTableData.tableHeadings}
         tableData={itemTableData.tableData}
-        mt="s"
-        mb="s"
-        sx={{
-          width: "unset",
-          thead: { mb: "xs" },
-          tr: { border: "0 !important" },
-          "th, td": {
-            width: "240px",
-            padding: "0",
-            marginRight: "32px",
-          },
-          "th, td, th > span, td > span": {
-            paddingTop: "0",
-            paddingBottom: "0",
-          },
-        }}
       />
       {!itemTableData.isBibPage && (
         <Box>
