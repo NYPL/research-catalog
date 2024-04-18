db7ca4d5aba8b89619906c64864ebfebc7d00c76
Diego Cohen <diego.cohen3@gmail.com>
2024-04-18 12:54:02 -0400

    Fix search sort tests

--- src/components/SearchResults/SearchResultsSort.test.tsx
+++ src/components/SearchResults/SearchResultsSort.test.tsx
@@ -6,30 +6,11 @@ import userEvent from "@testing-library/user-event"
 describe("SearchResultsSort", () => {
   it("calls the callback function when changed", async () => {
     const onChange = jest.fn()
-    render(
-      <SearchResultsSort
-        pageHasResults={true}
-        searchParams={{}}
-        handleSortChange={onChange}
-      />
-    )
+    render(<SearchResultsSort searchParams={{}} handleSortChange={onChange} />)
     const sortSelect = screen.getByLabelText("Sort by")
     expect(sortSelect).toBeInTheDocument()
     await userEvent.selectOptions(sortSelect, "Title (A - Z)")
     expect(sortSelect).toHaveFocus()
     expect(onChange).toHaveBeenCalled()
-  })
-
-  it("does not render when there are no search results on the page", async () => {
-    const onChange = jest.fn()
-    render(
-      <SearchResultsSort
-        pageHasResults={false}
-        searchParams={{}}
-        handleSortChange={onChange}
-      />
-    )
-    const sortSelect = screen.queryByLabelText("Sort by")
-    expect(sortSelect).not.toBeInTheDocument()
   })
 })
