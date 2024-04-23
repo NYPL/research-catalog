ee512c7c66c9928af9d0568b8f64f52262bc9ab1
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2024-04-23 10:35:10 -0700

    Removing margins

--- src/components/MyAccount/FeesBanner.tsx
+++ src/components/MyAccount/FeesBanner.tsx
@@ -4,14 +4,14 @@ import { appConfig } from "../../config/config"
 const FeesBanner = () => {
   return (
     <Banner
-      sx={{ "> div ": { maxWidth: "unset" }, marginBottom: "m" }}
+      sx={{ marginBottom: "m" }}
       type="negative"
       content={
         <>
           <Text sx={{ marginBottom: 0 }}>
             You have outstanding fees. Borrowing privileges will be suspended
-            for cardholders with replacement fees totaling $100 or more. <br />{" "}
-            Fees can be paid at any New York Public Library branch in cash, U.S.
+            for cardholders with replacement fees totaling $100 or more. Fees
+            can be paid at any New York Public Library branch in cash, U.S.
             Postal money order, personal check, or{" "}
             <Link
               href={`${appConfig.urls.circulatingCatalog}?openAccount=fines-and-fees`}
