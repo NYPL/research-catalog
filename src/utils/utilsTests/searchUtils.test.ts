1219e491fd993e80e51494b08fe3becdf26e833b
Edwin Guzman <edwin.gzmn@gmail.com>
2024-04-25 09:55:02 -0400

    Updating the display string text for different select drop down options

--- src/utils/utilsTests/searchUtils.test.ts
+++ src/utils/utilsTests/searchUtils.test.ts
@@ -7,6 +7,7 @@ import {
   getFreshSortByQuery,
 } from "../searchUtils"
 import { queryParamsEquality } from "../../../__test__/helpers/searchHelpers"
+import { type SearchParams } from "../../types/searchTypes"
 
 const checkQueryParamsEquality = queryParamsEquality(getSearchQuery)
 
@@ -60,17 +61,24 @@ describe("searchUtils", () => {
           search_scope: "contributor",
           sort_direction: "asc",
           sort: "relevance",
+          contributor: "",
         })
       ).toEqual({
         identifiers: {
           issn: "123",
           isbn: "456",
+          lccn: undefined,
+          oclc: undefined,
         },
         page: 1,
         q: "",
         field: "contributor",
         order: "asc",
         sortBy: "relevance",
+        subject: undefined,
+        title: undefined,
+        contributor: "",
+        filters: undefined,
       })
     })
     it("parses the page number query string value into a number", () => {
@@ -81,6 +89,14 @@ describe("searchUtils", () => {
       ).toEqual({
         page: 2,
         q: "",
+        field: undefined,
+        order: undefined,
+        sortBy: undefined,
+        subject: undefined,
+        title: undefined,
+        contributor: undefined,
+        filters: undefined,
+        identifiers: undefined,
       })
     })
   })
@@ -130,6 +146,12 @@ describe("searchUtils", () => {
       )
       expect(heading.toLocaleLowerCase().includes("keyword")).toBe(false)
     })
+    it("displays the default keyword display string", () => {
+      const heading = getSearchResultsHeading({ q: "spaghetti" }, 100)
+      expect(heading).toEqual(
+        'Displaying 1-50 of 100 results for keyword "spaghetti"'
+      )
+    })
     it("displays all of the values from advanced search and nothing else", () => {
       const heading = getSearchResultsHeading(
         {
@@ -144,6 +166,20 @@ describe("searchUtils", () => {
       )
       expect(heading).toEqual(
         'Displaying 1-50 of 100 results for keyword "spaghetti" and title "ricotta" and author "pasta mama" and subject "italian"'
+      )
+    })
+    it("displays the appropriate string for certain values", () => {
+      const heading = getSearchResultsHeading(
+        {
+          page: 1,
+          q: "spaghetti",
+          field: "journal_title",
+          journal_title: "spaghetti",
+        } as SearchParams,
+        100
+      )
+      expect(heading).toEqual(
+        'Displaying 1-50 of 100 results for journal title "spaghetti"'
       )
     })
     it("returns the correct heading string for first page", () => {
