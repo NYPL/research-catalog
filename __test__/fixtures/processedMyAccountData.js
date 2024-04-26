f23811c166f1c51f2dcd3a406984567df0e08ede
Vera Kahn <verakahn@nypl.org>
2024-04-26 10:16:58 -0400

    fix types

--- __test__/fixtures/processedMyAccountData.js
+++ __test__/fixtures/processedMyAccountData.js
@@ -20,10 +20,7 @@ export const emptyPatron = {
   expirationDate: "2025-03-28",
   emails: [],
   phones: [],
-  homeLibrary: {
-    name: "Stavros Niarchos Foundation Library (SNFL)",
-    code: "sn   ",
-  },
+  homeLibraryCode: "sn   ",
   id: 2772226,
   notificationPreference: null,
 }
