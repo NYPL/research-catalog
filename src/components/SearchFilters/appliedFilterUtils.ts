c5ab599741e285ef2d06b644ba0167cc1356ebec
Vera Kahn <verakahn@nypl.org>
2024-05-10 10:52:23 -0400

    empty option bug fix

--- src/components/SearchFilters/appliedFilterUtils.ts
+++ src/components/SearchFilters/appliedFilterUtils.ts
@@ -53,33 +53,37 @@ export const addLabelPropAndParseFilters = (
       continue
     appliedFilterValuesWithLabels[appliedFilterField] = appliedFilterValues[
       appliedFilterField
-    ].map((filterValue: string): Option => {
-      // dateBefore and dateAfter fields are not based on
-      // aggregations results. Pass the year along with out
-      // transforming fieldname or finding the label
-      if (appliedFilterField.includes("date")) {
-        const labelPrefix = appliedFilterField.split("date")[1]
-        return {
-          count: null,
-          value: filterValue,
-          label: `${labelPrefix} ${filterValue}`,
+    ]
+      .map((filterValue: string): Option => {
+        // dateBefore and dateAfter fields are not based on
+        // aggregations results. Pass the year along with out
+        // transforming fieldname or finding the label
+        if (appliedFilterField.includes("date")) {
+          const labelPrefix = appliedFilterField.split("date")[1]
+          return {
+            count: null,
+            value: filterValue,
+            label: `${labelPrefix} ${filterValue}`,
+          }
         }
-      }
-      // Subject literals can be combinations of multiple subjects, ie a -- b -- c.
-      // We need special handling for when a query is made for a -- b, but
-      // aggregations only returns a -- b -- c.
-      if (appliedFilterField === "subjectLiteral")
-        return {
-          count: null,
-          value: filterValue,
-          label: filterValue,
-        }
-      // Find the option with the same value, so we can eventually display the label
-      const matchingOption = matchingFieldAggregation.values.find(
-        (option: Option) => option.value === filterValue
-      )
-      return matchingOption
-    })
+        // Subject literals can be combinations of multiple subjects, ie a -- b -- c.
+        // We need special handling for when a query is made for a -- b, but
+        // aggregations only returns a -- b -- c.
+        if (appliedFilterField === "subjectLiteral")
+          return {
+            count: null,
+            value: filterValue,
+            label: filterValue,
+          }
+        // Find the option with the same value, so we can eventually display the label
+        const matchingOption = matchingFieldAggregation.values.find(
+          (option: Option) => option.value === filterValue
+        )
+        return matchingOption
+      })
+      // don't pass on falsy options. this happens when no aggregations are
+      // returned for a given param, and possibly in other scenarios.
+      .filter((option) => option)
   }
   delete appliedFilterValuesWithLabels["q"]
   return appliedFilterValuesWithLabels
@@ -89,27 +93,16 @@ export const buildTagsetData = (
   appliedFiltersWithLabels: Record<string, Option[]>
 ) => {
   const appliedFilterFields = Object.keys(appliedFiltersWithLabels)
-  return (
-    appliedFilterFields
-      .map((field: string) => {
-        const appliedFiltersWithLabelsPerField = appliedFiltersWithLabels[field]
-        // HOTFIX 5/9/24 - this is a temporary fix to resolve a 500 error on production caused by certain combinations of filters. Discovery API is bumping off certain resourcetypes from the aggregations.
-        // Specifically on this query: local.nypl.org:8080/research/research-catalog/search?q=pop&filters[materialType][0]=resourcetypes%3Acar&filters[materialType][1]=resourcetypes%3Amul&filters[materialType][2]=resourcetypes%3Acar&filters[materialType][3]=resourcetypes%3Aaud&filters[materialType][4]=resourcetypes%3Anot&filters[materialType][5]=resourcetypes%3Amix&filters[materialType][6]=resourcetypes%3Amul&filters[materialType][7]=resourcetypes%3Amov&filters[language][0]=lang%3Aeng&filters[language][1]=lang%3Ager&filters[language][2]=lang%3Akor&filters[language][3]=lang%3Afre&filters[subjectLiteral][0]=Pop+music&filters[subjectLiteral][1]=Popular+music&filters[subjectLiteral][2]=Music+--+Social+aspects.&filters[subjectLiteral][3]=Pop+art+--+United+States.&filters[subjectLiteral][4]=Art%2C+Modern+--+20th+century+--+Exhibitions.&filters[subjectLiteral][5]=Popular+music+--+United+States+--+History+and+criticism.&filters[subjectLiteral][6]=Manners+and+customs
-        // resourcetype:car and resourcetype:mul are not returned in the aggregations.
-        // TODO: Find the root cause of this issue and remove this override
-        if (appliedFiltersWithLabelsPerField.some((filter) => !filter))
-          return null
-        return appliedFiltersWithLabelsPerField.map((filter: Option) => {
-          return {
-            id: field + "-" + filter?.label,
-            label: filter?.label,
-            field,
-          }
-        })
+  return appliedFilterFields
+    .map((field: string) => {
+      const appliedFiltersWithLabelsPerField = appliedFiltersWithLabels[field]
+      return appliedFiltersWithLabelsPerField.map((filter: Option) => {
+        return {
+          id: field + "-" + filter?.label,
+          label: filter?.label,
+          field,
+        }
       })
-      // HOTFIX 5/9/24
-      // See comment above
-      .filter((filterArr) => !!filterArr)
-      .flat()
-  )
+    })
+    .flat()
 }
