b0fa805c36e32345ab1545ab317456c9f142a52a
Vera Kahn <verakahn@nypl.org>
2024-06-03 17:41:32 -0400

    extract modal props

---
+++ src/components/MyAccount/Settings/SuccessAndFailureModalProps.tsx
@@ -0,0 +1,53 @@
+import {
+  Box,
+  Heading,
+  Icon,
+  Link,
+  Text,
+} from "@nypl/design-system-react-components"
+import styles from "../../../../styles/components/MyAccount.module.scss"
+
+export const successModalProps = {
+  type: "default",
+  bodyContent: (
+    <Box className={styles.modalBody}>
+      <Text sx={{ marginLeft: "l" }}>
+        Your account settings were successfully updated.
+      </Text>
+    </Box>
+  ),
+  closeButtonLabel: "OK",
+  headingText: (
+    <Heading className={styles.modalHeading}>
+      <>
+        <Icon
+          size="large"
+          name="actionCheckCircleFilled"
+          color="ui.success.primary"
+        />
+        <Text sx={{ marginBottom: 0 }}> Update successful </Text>
+      </>
+    </Heading>
+  ),
+}
+export const failureModalProps = {
+  type: "default",
+  bodyContent: (
+    <Box className={styles.modalBody}>
+      <Text sx={{ marginLeft: "l", marginRight: "m" }}>
+        We were unable to update your account settings. Please try again or{" "}
+        <Link href="https://www.nypl.org/get-help/contact-us">contact us</Link>{" "}
+        for assistance.
+      </Text>
+    </Box>
+  ),
+  closeButtonLabel: "OK",
+  headingText: (
+    <Heading className={styles.modalHeading}>
+      <>
+        <Icon size="large" name="errorFilled" color="ui.error.primary" />
+        <Text sx={{ marginBottom: 0 }}> Update failed </Text>
+      </>
+    </Heading>
+  ),
+}
