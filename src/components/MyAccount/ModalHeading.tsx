1996625b8d0d5de326dbc4219e32b7fa7d78bac7
Vera Kahn <verakahn@nypl.org>
2024-06-06 14:02:36 -0400

    add comment

--- src/components/MyAccount/ModalHeading.tsx
+++ src/components/MyAccount/ModalHeading.tsx
@@ -1,6 +1,10 @@
 import { Heading } from "@nypl/design-system-react-components"
 import styles from "../../../styles/components/MyAccount.module.scss"
 
+// this method is a workaround because the size of the heading can't be updated
+// from the css module. it's a method because the headingText prop of the modal
+// props object requires ONLY a Heading component and won't accept a custom
+// component to be passed in.
 const modalHeading = (inner) => {
   let className = styles.modalHeading
   // most modal headings have an icon. the few that don't need extra padding to make up for the space the icon takes up.
