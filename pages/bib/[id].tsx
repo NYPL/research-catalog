3006f802c75db6a38b24a8984f65e1ad28b5d451
Diego Cohen <diego.cohen3@gmail.com>
2024-06-07 11:31:03 -0400

    Remove console log

--- pages/bib/[id].tsx
+++ pages/bib/[id].tsx
@@ -47,7 +47,6 @@ export default function BibPage({
   isAuthenticated,
   itemPage = 1,
 }: BibPropsType) {
-  console.log(discoveryBibResult)
   const { pathname, push, query } = useRouter()
   const metadataTitle = `Item Details | ${SITE_NAME}`
   const bib = new Bib(discoveryBibResult)
