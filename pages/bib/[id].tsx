89e589a40c556ff405124d8be933839ff6e98e34
Diego Cohen <diego.cohen3@gmail.com>
2024-06-13 15:26:20 -0400

    Set page in state to update in handlePageChange

--- pages/bib/[id].tsx
+++ pages/bib/[id].tsx
@@ -32,6 +32,7 @@ import Bib from "../../src/models/Bib"
 import initializePatronTokenAuth from "../../src/server/auth"
 import Item from "../../src/models/Item"
 import type { SearchResultsItem } from "../../src/types/itemTypes"
+import type { ParsedUrlQueryInput } from "querystring"
 
 interface BibPropsType {
   discoveryBibResult: DiscoveryBibResult
@@ -56,6 +57,7 @@ export default function BibPage({
   const [itemsLoading, setItemsLoading] = useState(false)
   const [itemFetchError, setItemFetchError] = useState(bib.showItemTableError)
   const [bibItems, setBibItems] = useState(bib.items)
+  const [itemTablePage, setItemTablePage] = useState(itemPage)
   const itemTableScrollRef = useRef<HTMLDivElement>(null)
 
   const { topDetails, bottomDetails, holdingsDetails } = new BibDetailsModel(
@@ -70,10 +72,14 @@ export default function BibPage({
   const refreshItemTable = async (newQuery: BibQueryParams) => {
     setItemsLoading(true)
     setItemFetchError(false)
-    await push({ pathname, query: { ...newQuery } }, undefined, {
-      shallow: true,
-    })
-    const bibQueryString = getBibQueryString(newQuery)
+    await push(
+      { pathname, query: newQuery as ParsedUrlQueryInput },
+      undefined,
+      {
+        shallow: true,
+      }
+    )
+    const bibQueryString = getBibQueryString(query)
     const response = await fetch(
       `${BASE_URL}/api/bib/${bib.id}/items?${bibQueryString}`
     )
@@ -91,6 +97,7 @@ export default function BibPage({
   }
 
   const handlePageChange = async (page: number) => {
+    setItemTablePage(page)
     const newQuery = { ...query, item_page: page }
     if (page === 1) delete newQuery.item_page
     await refreshItemTable(newQuery)
@@ -138,7 +145,7 @@ export default function BibPage({
                     mb={{ base: "s", md: "m" }}
                   >
                     {buildItemTableDisplayingString(
-                      itemPage,
+                      itemTablePage,
                       bib.numPhysicalItems
                     )}
                   </Heading>
@@ -147,8 +154,8 @@ export default function BibPage({
               )}
               <Pagination
                 id="bib-items-pagination"
-                initialPage={itemPage}
-                currentPage={itemPage}
+                initialPage={itemTablePage}
+                currentPage={itemTablePage}
                 pageCount={Math.ceil(bib.numPhysicalItems / ITEM_BATCH_SIZE)}
                 onPageChange={handlePageChange}
                 my="xl"
