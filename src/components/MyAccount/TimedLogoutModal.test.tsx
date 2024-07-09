22454f337467ee4d8185bc91ba5142d545323861
Vera Kahn <verakahn@nypl.org>
2024-07-09 14:28:18 -0400

    draft tests

--- src/components/MyAccount/TimedLogoutModal.test.tsx
+++ src/components/MyAccount/TimedLogoutModal.test.tsx
@@ -6,32 +6,8 @@ jest.mock("next/router", () => jest.requireActual("next-router-mock"))
 jest.useFakeTimers().setSystemTime(new Date("Mon, 08 Jul 2024 12:00:00 GMT"))
 
 describe("TimedLogoutModal", () => {
-  xit("does not render if expiration is empty string", () => {
-    render(
-      <TimedLogoutModal
-        expirationTime={""}
-        stayLoggedIn={() => {
-          return
-        }}
-      />
-    )
-    const heading = screen.queryByText("Your session is about to expire")
-    expect(heading).not.toBeInTheDocument()
-  })
-  it("does not render until the timeout window has ellapsed", async () => {
-    render(
-      <TimedLogoutModal
-        expirationTime={"Mon, 08 Jul 2024 12:01:00 GMT"}
-        stayLoggedIn={() => {
-          return
-        }}
-        timeoutWindow={1}
-      />
-    )
-    await setTimeout(() => {
-      console.log("timeout")
-      const heading = screen.queryByText("Your session is about to expire")
-      expect(heading).not.toBeInTheDocument()
-    }, 61 * 1000)
-  })
+  it.todo("does not render if expiration is empty string")
+  it.todo("does not render until the timeout window has ellapsed")
+  it.todo("closes and redirects when time till expiration is zero")
+  it.todo("returns to account page when user clicks stay logged in")
 })
