4604ba908ea422c9b1f47d303233086bfb406da9
Vera Kahn <verakahn@nypl.org>
2024-07-18 11:46:43 -0400

    use fetchPatron in getserversideprops

--- pages/account/[[...index]].tsx
+++ pages/account/[[...index]].tsx
@@ -11,6 +11,7 @@ import ProfileContainer from "../../src/components/MyAccount/ProfileContainer"
 import sierraClient from "../../src/server/sierraClient"
 import type { MyAccountPatronData } from "../../src/types/myAccountTypes"
 import { PatronDataProvider } from "../../src/context/PatronDataContext"
+import { fetchPatronData } from "../api/account/[id]"
 interface MyAccountPropsType {
   accountData: MyAccountPatronData
   isAuthenticated: boolean
@@ -83,9 +84,11 @@ export async function getServerSideProps({ req, res }) {
   const tabsPath = match ? match[1] : null
   const id = patronTokenResponse.decodedPatron.sub
   try {
-    const client = await sierraClient()
-    const { checkouts, holds, patron, fines, pickupLocations } =
-      await MyAccountFactory(id, client)
+    const patronData = await fetchPatronData(id)
+    const { checkouts, holds, patron, fines, pickupLocations } = JSON.parse(
+      patronData.message
+    )
+
     /*  Redirecting invalid paths (including /overdues if user has none) and
     // cleaning extra parts off valid paths. */
     if (tabsPath) {
