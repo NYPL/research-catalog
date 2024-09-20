44d7a2e974304f99683dea5a262b76809073836a
Vera Kahn <verakahn@nypl.org>
2024-09-20 10:40:04 -0400

    update pickup time estimator

--- __test__/pages/test-pickup-times/index.test.tsx
+++ __test__/pages/test-pickup-times/index.test.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { screen, render } from "../../../src/utils/testUtils"
 
-// import TestPickupTimes from "../../../pages/test-pickup-times/index"
+import TestPickupTimes from "../../../pages/test-pickup-times/index"
 
 jest.mock("next/router", () => jest.requireActual("next-router-mock"))
 
-xdescribe("Test Pickup Times", () => {
+describe("Test Pickup Times", () => {
   describe("Displays testing form", () => {
     it("displays testing form", () => {
-      // render(<TestPickupTimes />)
+      render(<TestPickupTimes />)
 
       const forms = screen.getAllByRole("form")
       expect(forms).toHaveLength(1)
