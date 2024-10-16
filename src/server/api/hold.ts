4a0f790cc676306174de22b67f2bf0b564f67b1b
Diego Cohen <diego.cohen3@gmail.com>
2024-10-16 16:06:50 -0400

    Initialize hold api file

---
+++ src/server/api/hold.ts
@@ -0,0 +1,11 @@
+/**
+ * Getter function for hold delivery locations.
+ */
+export async function fetchDeliveryLocations(
+  barcode: string,
+  patronId: string
+): string {
+  console.log(barcode)
+  console.log(patronId)
+  return ""
+}
