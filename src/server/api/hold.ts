8efee9eff5f75795928638b143861172b9d47171
Diego Cohen <diego.cohen3@gmail.com>
2024-10-16 20:53:42 -0400

    Integrate nypl client into fetchDeliveryLocations

--- src/server/api/hold.ts
+++ src/server/api/hold.ts
@@ -1,11 +1,19 @@
+import nyplApiClient from "../nyplApiClient"
+
 /**
  * Getter function for hold delivery locations.
  */
 export async function fetchDeliveryLocations(
   barcode: string,
   patronId: string
-): string {
-  console.log(barcode)
-  console.log(patronId)
-  return ""
+): Promise<string> {
+  const deliveryEndpoint = `/request/deliveryLocationsByBarcode?barcodes[]=${barcode}&patronId=${patronId}`
+  try {
+    const client = await nyplApiClient()
+    const { data } = await client.get(deliveryEndpoint)
+    return data
+  } catch (error) {
+    console.error(`Error fetching delivery locations ${error.message}`)
+    throw new Error(error)
+  }
 }
