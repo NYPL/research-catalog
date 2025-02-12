93871c29de61803c30604b113a38dbe2de300664
Diego Cohen <diego.cohen3@gmail.com>
2025-02-12 10:43:57 -0500

    Update changelog and add comment

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@
 const nextConfig = {
   reactStrictMode: false,
   basePath: "/research/research-catalog",
-  // pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
-  webpack(config, { isServer }) {
+  /**
+   * Prevents Webpack from polyfilling 'fs' in client builds.
+   * This avoids errors when server-side functions using Winston
+   * are imported in page components with getServerSideProps.
+   */
+  webpack: (config, { isServer }) => {
     if (!isServer) {
-      // Ignore 'fs' module in the client-side code
       config.resolve.fallback = {
         fs: false,
       }
