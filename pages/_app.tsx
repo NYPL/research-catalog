6ad3202f73bc23f388c8d389692e8703ebb3c958
Diego Cohen <diego.cohen3@gmail.com>
2025-02-20 10:23:43 -0500

    Restore advocacy script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,6 +50,11 @@ function App({ Component, pageProps }) {
               });
            `}
       </Script>
+      {/*  NYPL Advocacy Snippet for Marketing Campaigns */}
+      <Script
+        src="https://assets.nypl.org/js/advocacy.js"
+        strategy="afterInteractive"
+      />
       {/* NYPL Header script */}
       <Script
         src={`${
