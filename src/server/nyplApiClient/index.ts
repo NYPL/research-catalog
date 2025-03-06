35523f710a6dd43c2a00682c98210f3cc4858dc0
Vera Kahn <verakahn@nypl.org>
2025-03-06 17:06:41 -0500

    update logs

--- src/server/nyplApiClient/index.ts
+++ src/server/nyplApiClient/index.ts
@@ -66,7 +66,7 @@ const nyplApiClient = async ({
     }
     const post = nyplApiClient.post.bind(nyplApiClient)
     nyplApiClient.post = async function (path, body) {
-      logger.info(`POST ${baseUrl}/${path}`)
+      logger.info(`POSTing ${body} to ${baseUrl}/${path}`)
       return post(path, body)
     }
     return nyplApiClient
