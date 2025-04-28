d5e8c087c1210c108b2ec0e283fbf1850e8ee04e
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-04-28 18:53:18 -0400

    Actually fixing tests

--- src/components/RefineSearch/RefineSearch.test.tsx
+++ src/components/RefineSearch/RefineSearch.test.tsx
@@ -232,13 +232,10 @@ describe("RefineSearch", () => {
         },
       })
       await openRefineSearch()
-      // this should actually deselect the filters as it is just clicking on filters
-      await userEvent.click(
-        screen.getByRole("checkbox", { name: "Italian (59)" })
-      )
-      await userEvent.click(
-        screen.getByRole("checkbox", { name: "Audio (37)" })
-      )
+
+      await userEvent.click(screen.getByText("Italian (59)"))
+      await userEvent.click(screen.getByText("Audio (37)"))
+
       await apply()
       expect(mockRouter.query).toStrictEqual({
         "filters[language][0]": "lang:por",
