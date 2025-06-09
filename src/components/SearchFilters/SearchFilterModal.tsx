9349109754753c2a94f5efab5c60c01a48a51fc7
Vera Kahn <verakahn@nypl.org>
2025-06-09 12:17:06 -0400

    use chakra modal focus handling

--- src/components/SearchFilters/SearchFilterModal.tsx
+++ src/components/SearchFilters/SearchFilterModal.tsx
@@ -5,7 +5,7 @@ import {
   Box,
   Heading,
 } from "@nypl/design-system-react-components"
-import { useEffect, useState } from "react"
+import { useRef, useState } from "react"
 import type { Aggregation } from "../../types/filterTypes"
 import {
   Modal as ChakraModal,
@@ -13,6 +13,8 @@ import {
   ModalContent,
   ModalFooter,
   ModalHeader,
+  useDisclosure,
+  ModalCloseButton,
 } from "@chakra-ui/react"
 import router from "next/router"
 import { getQueryWithoutFiltersOrPage } from "../../utils/refineSearchUtils"
@@ -27,72 +29,66 @@ const SearchFilterModal = ({
   aggregations: Aggregation[]
   searchResultsCount?: number
 }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const { setPersistentFocus } = useFocusContext()
-
-  const closeModal = () => {
-    setPersistentFocus(idConstants.searchFiltersModal)
-    setIsModalOpen(false)
-  }
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const handleClear = () => {
     router.push({
       pathname: "/search",
       query: getQueryWithoutFiltersOrPage(router.query),
     })
-    closeModal()
+    onClose()
   }
-
+  const finalRef = useRef()
   return (
     <>
-      {!isModalOpen && (
-        <Button
-          minWidth="44px"
-          width="100%"
-          onClick={() => setIsModalOpen(true)}
-          id="search-filters-modal"
-          display={{ base: "flex", md: "none" }}
-        >
-          <Icon size="large" align="left" name="contentFilterList" />
-          <FilterCount />
-        </Button>
-      )}
+      <Button
+        // ref={finalRef}
+        id={idConstants.searchFiltersModal}
+        minWidth="44px"
+        width="100%"
+        onClick={onOpen}
+        display={{ base: "flex", md: "none" }}
+      >
+        <Icon size="large" align="left" name="contentFilterList" />
+        <FilterCount />
+      </Button>
 
-      {isModalOpen && (
-        <ChakraModal
-          id={"modal"}
-          scrollBehavior="inside"
-          size={{ base: "full", md: "xl" }}
-          isOpen={isModalOpen}
-          onClose={closeModal}
-          aria-labelledby="Filter results"
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <Box
-              bg="ui.bg.default"
-              display="flex"
-              flexDir="row"
-              alignItems="center"
-              justifyContent="space-between"
-              mb="s"
-              pl="xs"
-              pr="xs"
-              sx={{
-                borderBottom: "1px solid var(--ui-border-default, #BDBDBD)",
-              }}
-            >
-              <ModalHeader padding="0" flex="1">
-                <Heading
-                  size="heading5"
-                  paddingTop="s"
-                  paddingBottom="s"
-                  paddingLeft="m"
-                >
-                  Filter results
-                </Heading>
-              </ModalHeader>
-              <Button
+      <ChakraModal
+        id={"modal"}
+        scrollBehavior="inside"
+        size={{ base: "full", md: "xl" }}
+        isOpen={isOpen}
+        onClose={onClose}
+        aria-labelledby="Filter results"
+        // finalFocusRef={finalRef}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <Box
+            bg="ui.bg.default"
+            display="flex"
+            flexDir="row"
+            alignItems="center"
+            justifyContent="space-between"
+            mb="s"
+            pl="xs"
+            pr="xs"
+            sx={{
+              borderBottom: "1px solid var(--ui-border-default, #BDBDBD)",
+            }}
+          >
+            <ModalHeader padding="0" flex="1">
+              <Heading
+                size="heading5"
+                paddingTop="s"
+                paddingBottom="s"
+                paddingLeft="m"
+              >
+                Filter results
+              </Heading>
+            </ModalHeader>
+            <ModalCloseButton />
+            {/* <Button
                 aria-label="Close"
                 id="close-btn"
                 buttonType="text"
@@ -102,49 +98,43 @@ const SearchFilterModal = ({
               >
                 {" "}
                 <Icon name="close" size="large" color="ui.black" />
-              </Button>
-            </Box>
-            <Box
-              overflow="auto"
+              </Button> */}
+          </Box>
+          <Box
+            overflow="auto"
+            display="flex"
+            paddingLeft="l"
+            paddingRight="l"
+            paddingBottom="m"
+          >
+            <SearchFilters aggregations={aggregations} />
+          </Box>
+          <ModalFooter>
+            <ButtonGroup
+              width="100%"
               display="flex"
-              paddingLeft="l"
-              paddingRight="l"
-              paddingBottom="m"
+              flexDir="column"
+              gap="xs"
+              paddingTop="s"
             >
-              <SearchFilters aggregations={aggregations} />
-            </Box>
-            <ModalFooter>
-              <ButtonGroup
-                width="100%"
-                display="flex"
-                flexDir="column"
-                gap="xs"
-                paddingTop="s"
+              <Button id="show-results" onClick={onClose}>
+                {`Show ${
+                  searchResultsCount === 10000
+                    ? "over 10,000"
+                    : searchResultsCount.toLocaleString()
+                } results`}
+              </Button>
+              <Button
+                id="clear-filters"
+                buttonType="secondary"
+                onClick={() => handleClear()}
               >
-                <Button
-                  id="show-results"
-                  onClick={() => {
-                    closeModal()
-                  }}
-                >
-                  {`Show ${
-                    searchResultsCount === 10000
-                      ? "over 10,000"
-                      : searchResultsCount.toLocaleString()
-                  } results`}
-                </Button>
-                <Button
-                  id="clear-filters"
-                  buttonType="secondary"
-                  onClick={() => handleClear()}
-                >
-                  Clear all filters
-                </Button>
-              </ButtonGroup>
-            </ModalFooter>
-          </ModalContent>
-        </ChakraModal>
-      )}
+                Clear all filters
+              </Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalContent>
+      </ChakraModal>
     </>
   )
 }
