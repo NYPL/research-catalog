962d6c5f7ff35c90d0c402b76ccaf1fc8b508171
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-06-24 13:10:42 -0400

    Adding status in all tests

--- __test__/pages/search/searchResults.test.tsx
+++ __test__/pages/search/searchResults.test.tsx
@@ -14,7 +14,12 @@ describe("Search Results page", () => {
   describe("More than 50 bibs", () => {
     it("displays many bibs", async () => {
       await mockRouter.push(`/search?q=${query}`)
-      render(<SearchResults isAuthenticated={true} results={{ results }} />)
+      render(
+        <SearchResults
+          isAuthenticated={true}
+          results={{ results, status: 200 }}
+        />
+      )
 
       const displayingText = screen.getByText(
         `Displaying 1-50 of ${results.totalResults} results for keyword "${query}"`
@@ -26,7 +31,12 @@ describe("Search Results page", () => {
     })
     it("displays pagination and updates the router on page button clicks", async () => {
       await mockRouter.push(`/search?q=${query}`)
-      render(<SearchResults isAuthenticated={true} results={{ results }} />)
+      render(
+        <SearchResults
+          isAuthenticated={true}
+          results={{ results, status: 200 }}
+        />
+      )
       screen.getByLabelText("Pagination")
 
       const pageButton = screen.getByLabelText("Page 2")
@@ -35,7 +45,12 @@ describe("Search Results page", () => {
     })
     it("renders the sort select fields and updates the query string in the url on changes", async () => {
       await mockRouter.push(`/search?q=${query}`)
-      render(<SearchResults isAuthenticated={true} results={{ results }} />)
+      render(
+        <SearchResults
+          isAuthenticated={true}
+          results={{ results, status: 200 }}
+        />
+      )
       const mobileSortBy = screen.getAllByLabelText("Sort by")[0]
       expect(mobileSortBy).toHaveValue("relevance")
       await userEvent.selectOptions(mobileSortBy, "Title (A - Z)")
@@ -56,7 +71,12 @@ describe("Search Results page", () => {
     })
     it("returns the user to the first page on sorting changes", async () => {
       await mockRouter.push(`/search?q=${query}&page=2`)
-      render(<SearchResults isAuthenticated={true} results={{ results }} />)
+      render(
+        <SearchResults
+          isAuthenticated={true}
+          results={{ results, status: 200 }}
+        />
+      )
       const mobileSortBy = screen.getAllByLabelText("Sort by")[0]
       await userEvent.selectOptions(mobileSortBy, "Title (Z - A)")
 
