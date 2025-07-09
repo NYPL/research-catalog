123527f847bb3f757010ed68a8fd5dce603e201a
Chris Mulholland <chrismulholland@nypl.org>
2025-07-09 11:13:36 -0400

    removed unnecessary comments

--- playwright/tests/example.spec.ts
+++ playwright/tests/example.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from "@playwright/test"
 
-// write test to open baseURL and check if the page contains the text "Research Catalog"
 test("open baseURL and check for text", async ({ page }) => {
   await page.goto("/research/research-catalog")
   await expect(
