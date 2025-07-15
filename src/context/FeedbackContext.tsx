704d1ca90b4c0c8f1267f16bca3b55b287bf95a8
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-07-15 16:39:06 -0400

    Already has URL :)

--- src/context/FeedbackContext.tsx
+++ src/context/FeedbackContext.tsx
@@ -2,8 +2,6 @@ import React, { useState, createContext } from "react"
 
 import { useFeedbackBox } from "@nypl/design-system-react-components"
 import type { FeedbackContextType } from "../types/feedbackTypes"
-import { BASE_URL } from "../config/constants"
-import { useRouter } from "next/router"
 
 /**
  * Wrapper context component that controls state for the Feedback component
@@ -12,19 +10,8 @@ export const FeedbackContext = createContext<FeedbackContextType | null>(null)
 
 export const FeedbackProvider = ({ children, value }) => {
   const [itemMetadata, setItemMetadata] = useState(value?.itemMetadata || null)
-  const [requestedURL, setRequestedURL] = useState(value?.requestURL || null)
   const [isError, setError] = useState(value?.error || false)
-  const { FeedbackBox, isOpen, onOpen: boxOpen, onClose } = useFeedbackBox()
-
-  // When user opens feedback box, get their URL and add to email data
-  const router = useRouter()
-  const onOpen = () => {
-    if (router && router.asPath) {
-      const fullURL = `${BASE_URL}${router.asPath}`
-      setRequestedURL(fullURL)
-    }
-    boxOpen()
-  }
+  const { FeedbackBox, isOpen, onOpen, onClose } = useFeedbackBox()
 
   // When user opens feedback box from an error page "contact us" link,
   // set error flag on feedback box
@@ -42,8 +29,6 @@ export const FeedbackProvider = ({ children, value }) => {
         onClose,
         itemMetadata,
         setItemMetadata,
-        requestedURL,
-        setRequestedURL,
         isError,
         setError,
         openFeedbackFormWithError,
