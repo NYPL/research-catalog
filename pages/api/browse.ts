b4f1b45efc64dd45885e7eec50639ab0646be5f9
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-07-22 14:05:39 -0400

    Updating route for author browse type

--- pages/api/browse.ts
+++ pages/api/browse.ts
@@ -3,15 +3,29 @@ import { fetchSubjects } from "../../src/server/api/browse"
 import { mapQueryToBrowseParams } from "../../src/utils/browseUtils"
 
 /**
- * Default API route handler for Browse
- * Calls a helper function that maps the query params object to a BrowseParams object
- * It is then passed to fetchSubjects, which fetches the subjects and returns a JSON response
- * via its onSuccess callback on a successful fetch
+ * API route handler for Browse
+ * Determines which fetcher to use based on the `browse_type` query parameter.
+ * Supported types: "subjects" (default), "authors".
+ * Calls a helper function that maps the query params object to a BrowseParams object,
+ * returns a JSON response.
  */
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
+    const { browse_type: browseType } = req.query
     const browseParams = mapQueryToBrowseParams(req.query)
-    const response = await fetchSubjects(browseParams)
+
+    let response
+
+    switch (browseType) {
+      case "authors":
+        //response = await fetchAuthors(browseParams)
+        break
+      case "subjects":
+      default:
+        response = await fetchSubjects(browseParams)
+        break
+    }
+
     res.status(200).json(response)
   }
 }
