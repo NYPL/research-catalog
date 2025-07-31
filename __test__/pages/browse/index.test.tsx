83377b46f8706a623c66e0977af225b294bca534
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-07-31 17:11:52 -0400

    Banner and test

--- __test__/pages/browse/index.test.tsx
+++ __test__/pages/browse/index.test.tsx
@@ -1,16 +1,22 @@
 import Browse from "../../../pages/browse"
 import { render, screen } from "../../../src/utils/testUtils"
+import { discoverySubjectsResult } from "../../fixtures/subjectFixtures"
 
 jest.mock("next/router", () => jest.requireActual("next-router-mock"))
 
 describe("Browse index page", () => {
-  render(
-    <Browse
-      results={{ status: 200, totalResults: 0, subjects: [] }}
-      isAuthenticated={false}
-    />
-  )
-  it("renders the subnav", () => {
+  it("renders the subnav and banner", () => {
+    render(
+      <Browse
+        results={{
+          status: 200,
+          totalResults: 4,
+          subjects: discoverySubjectsResult,
+        }}
+        isAuthenticated={false}
+      />
+    )
     expect(screen.queryByText("Browse the Catalog")).toBeInTheDocument()
+    expect(screen.queryByRole("banner")).toBeInTheDocument()
   })
 })
