6ca1022df1024f004134aa0167b1b53fd6494866
Chris Mulholland <chrismulholland@nypl.org>
2025-08-12 15:17:36 -0400

    refactoring and moving header and footer tests

--- playwright/tests/Homepage/homepage.spec.ts
+++ playwright/tests/Homepage/homepage.spec.ts
@@ -8,27 +8,6 @@ test.beforeEach(async ({ page }) => {
   await page.goto("")
 })
 
-test.describe("Global Header", () => {
-  test("Verify global header elements appear on the Research Catalog home page", async () => {
-    await expect(rcHomePage.nypl_logo).toBeVisible()
-    await expect(rcHomePage.nypl_logo_img).toBeVisible()
-    await expect(rcHomePage.header_my_account).toBeVisible()
-    await expect(rcHomePage.header_locations).toBeVisible()
-    await expect(rcHomePage.header_library_card).toBeVisible()
-    await expect(rcHomePage.header_newsletter).toBeVisible()
-    await expect(rcHomePage.header_donate).toBeVisible()
-    await expect(rcHomePage.header_shop).toBeVisible()
-    await expect(rcHomePage.header_books).toBeVisible()
-    await expect(rcHomePage.header_research).toBeVisible()
-    await expect(rcHomePage.header_education).toBeVisible()
-    await expect(rcHomePage.header_events).toBeVisible()
-    await expect(rcHomePage.header_connect).toBeVisible()
-    await expect(rcHomePage.header_give).toBeVisible()
-    await expect(rcHomePage.header_get_help).toBeVisible()
-    await expect(rcHomePage.header_search).toBeVisible()
-  })
-})
-
 test.describe("Research Catalog Home Page", () => {
   test("Verify elements on the Research Catalog home page", async ({
     page,
@@ -78,29 +57,5 @@ test.describe("Research Catalog Home Page", () => {
     await expect(rcHomePage.services_heading_blurb).toContainText(
       "Explore services"
     )
-  })
-})
-
-test.describe("Global Footer", () => {
-  test("Verify global footer elements appear on the Research Catalog home page", async () => {
-    const links = await rcHomePage.page.locator("footer a").allTextContents()
-    console.log(links)
-    await expect(rcHomePage.footer_container).toBeVisible()
-    await expect(rcHomePage.footer_library_text_image).toBeVisible()
-    await expect(rcHomePage.footer_building_image).toBeVisible()
-    await expect(rcHomePage.footer_copyright_text).toBeVisible()
-    await expect(rcHomePage.footer_legal_text).toBeVisible()
-    await expect(rcHomePage.footer_accessibility).toBeVisible()
-    await expect(rcHomePage.footer_press).toBeVisible()
-    await expect(rcHomePage.footer_careers).toBeVisible()
-    await expect(rcHomePage.footer_space_rental).toBeVisible()
-    await expect(rcHomePage.footer_privacy_policy).toBeVisible()
-    await expect(rcHomePage.footer_other_policies).toBeVisible()
-    await expect(rcHomePage.footer_terms).toBeVisible()
-    await expect(rcHomePage.footer_governance).toBeVisible()
-    await expect(rcHomePage.footer_rules_regulations).toBeVisible()
-    await expect(rcHomePage.footer_about).toBeVisible()
-    await expect(rcHomePage.footer_language).toBeVisible()
-    await expect(rcHomePage.help_and_feedback).toBeVisible()
   })
 })
