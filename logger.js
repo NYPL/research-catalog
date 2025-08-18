90ea1d3f414ba90cc49a01131c467ce387f4ef0d
Vera Kahn <verakahn@nypl.org>
2025-08-18 11:42:41 -0400

    formattting

--- logger.js
+++ logger.js
@@ -1,17 +1,18 @@
 import winston from "winston"
 
 const initializeLogger = () => {
-  const format = process.env.APP_ENV !== "development"
-    ? winston.format.json()
-    // Locally, let's do colorized plaintext logging:
-    : winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
-    )
+  const format =
+    process.env.APP_ENV !== "development"
+      ? winston.format.json()
+      : // Locally, let's do colorized plaintext logging:
+        winston.format.combine(
+          winston.format.colorize(),
+          winston.format.simple()
+        )
   return winston.createLogger({
     level: process.env.LOG_LEVEL || "info",
     format,
-    transports: [new winston.transports.Console()]
+    transports: [new winston.transports.Console()],
   })
 }
 
