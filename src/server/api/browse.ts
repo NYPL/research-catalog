ac4f708cea0fc448ee6cffcd3e9582c6477c29b3
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-10-16 16:14:13 -0400

    Reworking browse along same lines

--- src/server/api/browse.ts
+++ src/server/api/browse.ts
@@ -12,37 +12,48 @@ import { getBrowseQuery } from "../../utils/browseUtils"
 
 export async function fetchSubjects(
   browseParams?: BrowseParams
-): Promise<DiscoverySubjectsResponse | { status: number; message?: string }> {
+): Promise<
+  DiscoverySubjectsResponse | { status: number; name?: string; error?: string }
+> {
   const browseQuery = getBrowseQuery(browseParams)
 
   try {
     // Failure to build client will throw from this:
     const client = await nyplApiClient()
-    const res = await client.get(
+    const results = await client.get(
       `${DISCOVERY_API_BROWSE_ROUTE}${browseQuery}&per_page=${SUBJECTS_PER_PAGE.toString()}`
     )
 
-    // Handle empty results (404)
-    if (res.status === 404 || res.subjects.length === 0) {
+    // Handle no results (404)
+    if (results?.totalResults === 0) {
+      return {
+        status: 404,
+        error: `No results found for ${DISCOVERY_API_BROWSE_ROUTE}${browseQuery}&per_page=${SUBJECTS_PER_PAGE.toString()}`,
+      }
+    }
+
+    // Handle general error
+    if (results.status) {
       logServerError(
         "fetchSubjects",
         `${
-          res.message ? res.message : "No subjects found"
-        } Request: ${DISCOVERY_API_BROWSE_ROUTE}${browseQuery}`
+          results.error && results.error
+        } Request: ${DISCOVERY_API_BROWSE_ROUTE}${browseQuery}&per_page=${SUBJECTS_PER_PAGE.toString()}`
       )
       return {
-        status: res.status ?? 404,
-        message: res.message ?? "No subjects found",
+        status: results.status,
+        name: results.name,
+        error: results.error,
       }
     }
 
     return {
       status: 200,
-      subjects: res.subjects,
-      totalResults: res.totalResults,
+      subjects: results.subjects,
+      totalResults: results.totalResults,
     }
   } catch (error: any) {
     logServerError("fetchSubjects", error.message)
-    return { status: 500, message: error.message }
+    return { status: 500, error: error.message }
   }
 }
