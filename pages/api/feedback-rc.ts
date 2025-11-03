011fe46519e80f6d42d9a092c6f8be2352394870
Vera Kahn <verakahn@nypl.org>
2025-11-03 16:34:57 -0500

    update to v3 sdk

--- pages/api/feedback-rc.ts
+++ pages/api/feedback-rc.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import aws from "aws-sdk"
+import { SES } from "@aws-sdk/client-ses"
 
 import { getEmailParams } from "../../src/utils/feedbackUtils"
 import { appConfig } from "../../src/config/config"
@@ -7,7 +7,6 @@ import { appConfig } from "../../src/config/config"
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     if (!req.body) res.status(400).json({ error: "Malformed request" })
-    aws.config.update({ region: "us-east-1" })
     const emailParams = getEmailParams(
       req.body,
       req.headers.referer,
@@ -15,9 +14,9 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       appConfig.sourceEmail
     )
     // Create the promise and SES service object
-    const sendPromise = new aws.SES({ apiVersion: "2010-12-01" })
-      .sendEmail(emailParams)
-      .promise()
+    const sendPromise = new SES({
+      region: "us-east-1",
+    }).sendEmail(emailParams)
 
     return sendPromise
       .then((data) => res.status(200).json(data))
--- src/server/kms.ts
+++ src/server/kms.ts
@@ -1,9 +1,7 @@
 import { DecryptCommand, KMS } from "@aws-sdk/client-kms"
 import { logServerError } from "../utils/appUtils"
 
-const kms: KMS = new KMS({
-  region: "us-east-1",
-})
+const kms: KMS = new KMS({ region: "us-east-1" })
 
 const decryptKMS = async (key: string): Promise<string | null> => {
   const params = {
