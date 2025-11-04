43f425043397461ef0a529bc8890948af4d22cbd
Vera Kahn <verakahn@nypl.org>
2025-11-04 11:41:15 -0500

    fix circular issue in feedback-rc

--- pages/api/feedback-rc.ts
+++ pages/api/feedback-rc.ts
@@ -3,6 +3,7 @@ import { SES } from "@aws-sdk/client-ses"
 
 import { getEmailParams } from "../../src/utils/feedbackUtils"
 import { appConfig } from "../../src/config/config"
+import logger from "../../logger"
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
@@ -13,14 +14,17 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       appConfig.libAnswersEmail,
       appConfig.sourceEmail
     )
-    // Create the promise and SES service object
-    const sendPromise = new SES({
-      region: "us-east-1",
-    }).sendEmail(emailParams)
+    try {
+      // Create the promise and SES service object
+      const data = await new SES({
+        region: "us-east-1",
+      }).sendEmail(emailParams)
 
-    return sendPromise
-      .then((data) => res.status(200).json(data))
-      .catch((error) => res.status(400).json({ error }))
+      return res.status(200).json(data)
+    } catch (error) {
+      logger.error("feedback-rc handler error: " + error.message)
+      return res.status(400).json({ error: error.message })
+    }
   }
 }
 
