d712c218525582dbf5d21bcd86c743aa4f395bbb
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-12-11 13:28:27 -0500

    Testing with sierra creds

--- playwright/tests/redirects.spec.ts
+++ playwright/tests/redirects.spec.ts
@@ -1,36 +1,40 @@
 import { test, expect } from "@playwright/test"
 
-const BASE_URL =
-  process.env.BASE_URL || "http://local.nypl.org:8080/research/research-catalog"
-const BASE_HOST = BASE_URL.replace("/research/research-catalog", "")
-
 test("legacy url redirects to base path", async ({ page }) => {
-  await page.goto(`${BASE_HOST}/research/collections/shared-collection-catalog`)
-  await expect(page).toHaveURL(BASE_URL)
+  await page.goto(
+    "http://localhost:8080/research/collections/shared-collection-catalog"
+  )
+  await expect(page).toHaveURL(
+    "http://localhost:8080/research/research-catalog"
+  )
 })
 
 test("legacy url plus path redirects to base path", async ({ page }) => {
   await page.goto(
-    `${BASE_HOST}/research/collections/shared-collection-catalog/test`
+    "http://localhost:8080/research/collections/shared-collection-catalog/test"
   )
-  await expect(page).toHaveURL(`${BASE_URL}/test`)
+  await expect(page).toHaveURL(
+    "http://localhost:8080/research/research-catalog/test"
+  )
 })
 
 test("exact mapped SHEP link redirects to browse", async ({ page }) => {
   await page.goto(
-    `${BASE_URL}/subject_headings/c47dda10-a6a1-49ba-81db-6e46722674af`
+    "http://localhost:8080/research/research-catalog/subject_headings/c47dda10-a6a1-49ba-81db-6e46722674af"
   )
   await expect(page).toHaveURL(
-    `${BASE_URL}/browse/subjects/Hebrew language -- Study and teaching.`
+    "http://localhost:8080/research/research-catalog/browse/subjects/Hebrew language -- Study and teaching."
   )
 })
 
 test("SHEP link with filter parameter redirects to browse index", async ({
   page,
 }) => {
-  await page.goto(`${BASE_URL}/subject_headings?filter=Bronx River`)
+  await page.goto(
+    "http://localhost:8080/research/research-catalog/subject_headings?filter=Bronx River"
+  )
   await expect(page).toHaveURL(
-    `${BASE_URL}/browse?q=Bronx River&search_scope=starts_with`
+    "http://localhost:8080/research/research-catalog/browse?q=Bronx River&search_scope=starts_with"
   )
 })
 
@@ -38,19 +42,29 @@ test("SHEP link with label parameter redirects to browse results", async ({
   page,
 }) => {
   await page.goto(
-    `${BASE_URL}/subject_headings/c47dda10-a6a1-49ba-81db-6e46722674af?label=Hello`
+    "http://localhost:8080/research/research-catalog/subject_headings/c47dda10-a6a1-49ba-81db-6e46722674af?label=Hello"
   )
-  await expect(page).toHaveURL(`${BASE_URL}/browse/subjects/Hello.`)
+  await expect(page).toHaveURL(
+    "http://localhost:8080/research/research-catalog/browse/subjects/Hello."
+  )
 })
 
 test("SHEP base url redirects to browse", async ({ page }) => {
-  await page.goto(`${BASE_URL}/subject_headings`)
-  await expect(page).toHaveURL(`${BASE_URL}/browse`)
+  await page.goto(
+    "http://localhost:8080/research/research-catalog/subject_headings"
+  )
+  await expect(page).toHaveURL(
+    "http://localhost:8080/research/research-catalog/browse"
+  )
 })
 
 test("SHEP link with unknown uuid/path redirects to browse", async ({
   page,
 }) => {
-  await page.goto(`${BASE_URL}/subject_headings/something-something`)
-  await expect(page).toHaveURL(`${BASE_URL}/browse`)
+  await page.goto(
+    "http://localhost:8080/research/research-catalog/subject_headings/something-something"
+  )
+  await expect(page).toHaveURL(
+    "http://localhost:8080/research/research-catalog/browse"
+  )
 })
