dae0c49f558efa7ef310cc0caf5ceb8324a99c09
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-12-11 14:55:52 -0500

    More trying to report

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   use: {
     baseURL: "http://localhost:8080/research/research-catalog",
     trace: "on-first-retry",
+    screenshot: "only-on-failure",
   },
 
   projects: [
