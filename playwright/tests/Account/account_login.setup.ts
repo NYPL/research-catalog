58909fe8a4036dc690e8f9c344f0edeaab849969
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-12-12 16:32:39 -0500

    More

--- playwright/tests/Account/account_login.setup.ts
+++ playwright/tests/Account/account_login.setup.ts
@@ -5,18 +5,20 @@ const password = process.env.QA_PASSWORD
 
 // Logs in the test patron and saves their storage state for the following Playwright tests
 setup("authenticate test patron", async () => {
-  const browser = await chromium.launch()
+  const browser = await chromium.launch({ headless: false })
   const page = await browser.newPage()
 
   await page.goto("")
+  console.log("Url rn:", page.url())
   await page.getByRole("link", { name: /my account/i }).click()
 
+  console.log("Url rn 2:", page.url())
   await page.getByLabel(/barcode/i).fill(username)
   await page.getByLabel(/pin/i).fill(password)
 
   await page.getByRole("button", { name: /submit/i }).click()
 
-  await page.waitForURL("**/account")
+  await page.waitForURL("**/account", { timeout: 15000 })
 
   await page.context().storageState({ path: "playwright/auth/user.json" })
 
