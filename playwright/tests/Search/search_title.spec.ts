d9bf433c687588de6452a373f9195c4215db7cfe
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2025-12-12 10:32:05 -0500

    Scrolling in title test

--- playwright/tests/Search/search_title.spec.ts
+++ playwright/tests/Search/search_title.spec.ts
@@ -4,9 +4,18 @@ import { SearchPage } from "../../pages/search_page"
 let searchPage: SearchPage
 const searchterm = "It happened in New York"
 
+// Scroll down screen height
+async function scrollOneScreen(page): Promise<void> {
+  await page.evaluate(() => {
+    window.scrollBy(0, window.innerHeight)
+  })
+  await page.waitForTimeout(500)
+}
+
 test.beforeEach(async ({ page }) => {
   searchPage = new SearchPage(page, searchterm, "title")
   await page.goto("")
+  await page.waitForLoadState("networkidle")
 })
 
 test.describe("Title Search", () => {
@@ -14,12 +23,16 @@ test.describe("Title Search", () => {
     page,
   }) => {
     await searchPage.searchFor(searchterm, "Title")
+
     await expect(searchPage.searchResultsHeading).toBeVisible({
-      timeout: 15000,
+      timeout: 30000,
     })
-    await page.screenshot({ path: "ci-failure.png", fullPage: true })
-    await expect(await searchPage.searchResultsTitle.count()).toBeGreaterThan(
-      10
-    )
+
+    // Scroll twice? to load results below the fold in CI
+    await scrollOneScreen(page)
+    await scrollOneScreen(page)
+
+    const resultCount = await searchPage.searchResultsTitle.count()
+    expect(resultCount).toBeGreaterThan(10)
   })
 })
