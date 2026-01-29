4c712541269e322011a6cdbe4e927eb97417c0ea
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-01-28 20:22:36 -0500

    gen temp config

--- lib/bootstrap.ts
+++ lib/bootstrap.ts
@@ -1,6 +1,7 @@
 import { config, logger } from "@nypl/node-utils"
 import fs from "fs"
 import path from "path"
+import yaml from "js-yaml"
 
 let initialized = false
 
@@ -8,18 +9,25 @@ export async function bootstrapConfig() {
   if (initialized) return
 
   const env = process.env.APP_ENV || "development"
-
   const configPath = path.join(process.cwd(), "config", `${env}.yaml`)
 
-  const exists = fs.existsSync(configPath)
-  console.log({ configPath, exists })
-
-  if (fs.existsSync(configPath)) {
-    await config.loadConfig(env)
-  } else {
-    console.error(`Config file not found at ${configPath}`)
-    throw new Error(`Config file not found at ${configPath}`)
+  if (!fs.existsSync(configPath)) {
+    console.log(`Generating temp config for ${env}`)
+    const tmpYaml = yaml.dump({
+      PLAINTEXT_VARIABLES: {
+        LOG_LEVEL: process.env.LOG_LEVEL || "info",
+        PLATFORM_API_CLIENT_ID: process.env.PLATFORM_API_CLIENT_ID,
+        PLATFORM_API_CLIENT_SECRET: process.env.PLATFORM_API_CLIENT_SECRET,
+        SIERRA_SECRET: process.env.SIERRA_SECRET,
+        SIERRA_BASE: process.env.SIERRA_BASE,
+        SIERRA_KEY: process.env.SIERRA_KEY,
+      },
+      ENCRYPTED_VARIABLES: {},
+    })
+    fs.writeFileSync(configPath, tmpYaml)
   }
+
+  await config.loadConfig(env)
 
   logger.initialize({
     level: config.getConfig().LOG_LEVEL || "info",
