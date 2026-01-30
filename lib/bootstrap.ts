d30d46cce1a6e8e8e102dcf9b85357796570c0d8
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-01-30 10:26:42 -0500

    logs

--- lib/bootstrap.ts
+++ lib/bootstrap.ts
@@ -1,5 +1,9 @@
 import { config, logger } from "@nypl/node-utils"
+import fs from "fs"
 let initialized = false
+
+const files = fs.existsSync("config") ? fs.readdirSync("config") : []
+console.log("Config files in bundle:", files)
 
 export async function bootstrapConfig() {
   if (initialized) return
