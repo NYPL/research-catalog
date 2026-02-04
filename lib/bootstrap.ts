6028b8151716318c72140eb98af666af57cb0819
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-02-04 11:04:02 -0500

    Testing new rc.1

--- lib/bootstrap.ts
+++ lib/bootstrap.ts
@@ -1,4 +1,5 @@
 import { config, logger } from "@nypl/node-utils"
+import configJson from "../config/vercel-config.json"
 import fs from "fs"
 let initialized = false
 
@@ -8,7 +9,11 @@ console.log("Config files in bundle:", files)
 export async function bootstrapConfig() {
   if (initialized) return
 
-  await config.loadConfig(process.env.APP_ENV || "development")
+  if (process.env.VERCEL) {
+    await config.loadConfigFromObject(configJson)
+  } else {
+    await config.loadConfig(process.env.APP_ENV || "development")
+  }
 
   logger.initialize({
     level: config.getConfig().LOG_LEVEL || "info",
