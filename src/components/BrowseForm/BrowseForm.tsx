45ab471a03c7bd26669d0f3c68492b67868e3430
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-02-09 14:20:46 -0500

    Bring out back button

--- src/components/BrowseForm/BrowseForm.tsx
+++ src/components/BrowseForm/BrowseForm.tsx
@@ -2,7 +2,6 @@ import {
   Box,
   Flex,
   Icon,
-  Link,
   SearchBar,
   Text,
 } from "@nypl/design-system-react-components"
@@ -19,6 +18,7 @@ import SearchFilterModal from "../SearchFilters/SearchFilterModal"
 import type { RCPage } from "../../types/pageTypes"
 import type { Aggregation } from "../../types/filterTypes"
 import { useBrowseContext } from "../../context/BrowseContext"
+import { BackButton } from "../BackButton/BackButton"
 
 const BrowseForm = ({
   activePage,
@@ -155,32 +155,7 @@ const BrowseForm = ({
         >
           {(activePage === "sh-results" ||
             activePage === "contributor-results") &&
-            backUrl && (
-              <Link
-                variant="buttonSecondary"
-                id="back-index"
-                width={{ base: "100%", md: "fit-content" }}
-                onClick={() => router.push(backUrl)}
-                gap="xxs"
-                mt="xxs"
-                background="white"
-                mb={{ base: "xs", md: 0 }}
-              >
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
-                    fill="#0069BF"
-                  />
-                </svg>
-                Back to index
-              </Link>
-            )}
+            backUrl && <BackButton router={router} backUrl={backUrl} />}
 
           {displayFilters &&
             (activePage === "sh-results" ||
--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -23,6 +23,7 @@ import type { Aggregation } from "../../types/filterTypes"
 import { collapseMultiValueQueryParams } from "../../utils/refineSearchUtils"
 import { getSearchQuery } from "../../utils/searchUtils"
 import type { RCPage } from "../../types/pageTypes"
+import { BackButton } from "../BackButton/BackButton"
 
 const SearchForm = ({
   activePage,
@@ -137,32 +138,7 @@ const SearchForm = ({
         >
           {(activePage === "sh-results" ||
             activePage === "contributor-results") &&
-            backUrl && (
-              <Link
-                variant="buttonSecondary"
-                id="back-index"
-                width={{ base: "100%", md: "fit-content" }}
-                onClick={() => router.push(backUrl)}
-                gap="xxs"
-                mt="xxs"
-                background="white"
-                mb={{ base: "xs", md: 0 }}
-              >
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
-                    fill="#0069BF"
-                  />
-                </svg>
-                Back to index
-              </Link>
-            )}
+            backUrl && <BackButton router={router} backUrl={backUrl} />}
           <Link
             className={styles.advancedSearch}
             href="/search/advanced"
