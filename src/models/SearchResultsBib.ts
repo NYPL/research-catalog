5334c3597c07a3393a00787c85fb3cdc73f1d212
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-02-18 10:45:22 -0500

    Removes SearchResultBib's yearPublished field from existing/displaying

--- src/models/SearchResultsBib.ts
+++ src/models/SearchResultsBib.ts
@@ -5,22 +5,14 @@ import { ITEMS_PER_SEARCH_RESULT } from "../config/constants"
 
 /**
  * The SearchResultsBib class contains the data and getter functions
- * for a single Search Results Bib entity.
+ * for a single bib as it appears in search results.
  *
- * Search Results returned from the API are mapped and initialized into
- * SearchResultsBib objects in the SearchResults component.
- *
- * Certain string fields (e.g. publicationStatement) returned from the API are
- * formatted as a single item in an array. These fields are extracted and parsed
- * into simple string attributes via the object's constructor.
  */
 export default class SearchResultsBib extends Bib {
-  yearPublished?: string
   publicationStatement?: string
 
   constructor(result: DiscoveryBibResult) {
     super(result)
-    this.yearPublished = this.getYearFromResult(result)
     this.publicationStatement = result.publicationStatement?.length
       ? result.publicationStatement[0]
       : null
@@ -47,15 +39,5 @@ export default class SearchResultsBib extends Bib {
     return this.hasPhysicalItems
       ? this.numPhysicalItems
       : this.numElectronicResources
-  }
-
-  getYearFromResult(result: DiscoveryBibResult) {
-    const { dateStartYear } = result
-    const displayStartYear: string =
-      dateStartYear === 999 ? "unknown" : dateStartYear?.toString()
-    if (dateStartYear) {
-      return displayStartYear
-    }
-    return null
   }
 }
