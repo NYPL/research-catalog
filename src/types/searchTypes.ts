b0d0a82d0b7d7f58334a31dbb5ee095ad625d262
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-02-20 16:42:45 -0500

    Making debug object optional

--- src/types/searchTypes.ts
+++ src/types/searchTypes.ts
@@ -60,7 +60,7 @@ export interface DiscoveryAggregationResults {
 export interface DiscoverySearchResults {
   totalResults: number
   itemListElement: DiscoverySearchResultsElement[]
-  debug: { parsed: string[] }
+  debug?: { parsed?: string[] }
 }
 
 export interface DiscoverySearchResultsElement {
