abb63ce2319f1348641f798526944c13e825dfd8
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-03-03 10:08:50 -0500

    More logging

--- lib/bootstrap.ts
+++ lib/bootstrap.ts
@@ -6,10 +6,10 @@ export async function bootstrapConfig() {
   if (initialized) return
 
   if (process.env.VERCEL) {
+    console.log("CONFIG KEYS", Object.keys(configJson))
     await config.loadConfigFromObject(configJson)
   } else {
     console.log("BOOTSTRAP START", Date.now())
-    console.log(process.env.NEXT_PUBLIC_APP_ENV, process.env.NODE_ENV)
     await config.loadConfig(process.env.NEXT_PUBLIC_APP_ENV || "development")
   }
 
