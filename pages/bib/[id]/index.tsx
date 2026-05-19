093ea2cf31328b631576cc3efdf69c6aa4d91ce7
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-05-19 12:50:21 -0400

    legacy catalog copy updates

--- pages/bib/[id]/index.tsx
+++ pages/bib/[id]/index.tsx
@@ -338,7 +338,7 @@ export default function BibPage({
                 width="max-content"
                 mt="s"
               >
-                View in legacy catalog
+                View in legacy catalog (available onsite only)
               </Link>
             ) : null}
             <Link
