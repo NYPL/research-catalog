3fff445c43f90aae29d071690508a8f11782c753
Kasey Wei <kaseywei@nypl.org>
2026-07-06 12:15:12 -0400

    remove unused import

--- __test__/pages/search/advancedSearchForm.test.tsx
+++ __test__/pages/search/advancedSearchForm.test.tsx
@@ -1,11 +1,5 @@
 import React from "react"
-import {
-  fireEvent,
-  render,
-  screen,
-  delay,
-  waitFor,
-} from "../../../src/utils/testUtils"
+import { fireEvent, render, screen, delay } from "../../../src/utils/testUtils"
 import mockRouter from "next-router-mock"
 import userEvent from "@testing-library/user-event"
 
