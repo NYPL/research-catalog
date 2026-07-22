75436dff8602f7ca0442ea0a1a3cdbcb66d488d2
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-07-22 15:28:39 -0400

    test 2 (emma version)

--- src/models/ItemAvailability.ts
+++ src/models/ItemAvailability.ts
@@ -1,3 +1,92 @@
+// import { AVAILABILITY_KEYS } from "../config/constants"
+// import type { ItemCollectionAccess } from "../types/itemTypes"
+
+// const {
+//   NOT_AVAILABLE_NYPL,
+//   NOT_AVAILABLE_PARTNER,
+//   AVAILABLE_SHELF,
+//   AVAILABLE_DESK,
+//   AVAILABLE_ONSITE_APPT_NO_AEON,
+//   AVAILABLE_ONSITE_APPT_AEON,
+//   AVAILABLE_OFFSITE,
+//   AVAILABLE_CLOSED_STACK_NO_BARCODE,
+//   AVAILABLE_CLOSED_STACK,
+//   AVAILABLE_GENERAL,
+// } = AVAILABILITY_KEYS
+
+// /* Availability keys describe an availability status, and correspond to the message displayed
+//  ** below the request buttons on an item. Note: Many of these do not display a
+//  ** message (see the corresponding component). */
+// class ItemAvailability {
+//   key: string
+//   isAvailable: boolean
+//   isReCAP: boolean
+//   aeonUrl: string
+//   collectionAccessType: ItemCollectionAccess
+//   isSpecRequestable?: boolean
+//   isOnsite: boolean
+//   hasBarcode: boolean
+//   isPartnerReCAP: boolean
+
+//   constructor({
+//     isAvailable,
+//     isReCAP,
+//     aeonUrl,
+//     collectionAccessType,
+//     isSpecRequestable,
+//     hasBarcode,
+//     isPartnerReCAP,
+//   }) {
+//     this.isReCAP = isReCAP
+//     this.isAvailable = isAvailable
+//     this.collectionAccessType = collectionAccessType
+//     this.aeonUrl = aeonUrl
+//     this.isOnsite = !this.isReCAP
+//     this.isSpecRequestable = isSpecRequestable
+//     this.hasBarcode = hasBarcode
+//     this.isPartnerReCAP = isPartnerReCAP
+//     this.key = this.buildKey()
+//   }
+//   buildKey() {
+//     // Not available
+//     if (this.isPartnerReCAP && !this.isAvailable) {
+//       return NOT_AVAILABLE_PARTNER
+//     }
+//     if (!this.isAvailable) {
+//       return NOT_AVAILABLE_NYPL
+//     }
+
+//     // General collections, available
+//     if (this.collectionAccessType === "desk") {
+//       return AVAILABLE_DESK
+//     }
+//     if (this.collectionAccessType === "shelf") {
+//       return AVAILABLE_SHELF
+//     }
+//     if (!this.hasBarcode && !this.aeonUrl) {
+//       return AVAILABLE_CLOSED_STACK_NO_BARCODE
+//     }
+
+//     // Offsite, available
+//     if (this.isReCAP) {
+//       return AVAILABLE_OFFSITE
+//     }
+
+//     // Special collections, available
+//     if (this.isSpecRequestable && this.aeonUrl && this.isOnsite) {
+//       return AVAILABLE_ONSITE_APPT_AEON
+//     }
+//     if (this.isSpecRequestable && !this.aeonUrl && this.isOnsite) {
+//       return AVAILABLE_ONSITE_APPT_NO_AEON
+//     }
+
+//     // Catch-all:
+//     return AVAILABLE_GENERAL
+//   }
+// }
+
+// export default ItemAvailability
+
 import { AVAILABILITY_KEYS } from "../config/constants"
 import type { ItemCollectionAccess } from "../types/itemTypes"
 
@@ -10,7 +99,6 @@ const {
   AVAILABLE_ONSITE_APPT_AEON,
   AVAILABLE_OFFSITE,
   AVAILABLE_CLOSED_STACK_NO_BARCODE,
-  AVAILABLE_CLOSED_STACK,
   AVAILABLE_GENERAL,
 } = AVAILABILITY_KEYS
 
@@ -56,14 +144,13 @@ class ItemAvailability {
       return NOT_AVAILABLE_NYPL
     }
 
-    // Reference, available
+    // General collections, available
     if (this.collectionAccessType === "desk") {
       return AVAILABLE_DESK
     }
     if (this.collectionAccessType === "shelf") {
       return AVAILABLE_SHELF
     }
-
     if (!this.hasBarcode && !this.aeonUrl) {
       return AVAILABLE_CLOSED_STACK_NO_BARCODE
     }
