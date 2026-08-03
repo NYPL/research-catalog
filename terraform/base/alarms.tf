e7f1c1c37d585dd78395bd1d1e7e47ed1b25e6b2
Emma Mansell <73774046+7emansell@users.noreply.github.com>
2026-08-03 15:49:02 -0400

    Add comment for devops alarms

--- terraform/base/alarms.tf
+++ terraform/base/alarms.tf
@@ -1,3 +1,15 @@
+/* 
+DevOps covers these metric alarms for the production, qa, and train environments:
+research-catalog-{env}_alb_health
+research-catalog-{env}_alb500_scale_up
+research-catalog-{env}_alb500_scale_dn
+research-catalog-{env}_memory
+research-catalog-{env}_cpu
+research-catalog-{env}-task-deploy
+research-catalog-{env}-min-task-count
+research-catalog-{env}-min-instance-count
+*/
+
 resource "aws_cloudwatch_metric_alarm" "research_catalog_error" {
   alarm_name = "ResearchCatalogErrorAlarm"
 
@@ -19,4 +31,5 @@ resource "aws_cloudwatch_metric_alarm" "research_catalog_error" {
   datapoints_to_alarm = 1
 
   treat_missing_data = "missing"
+  tags = local.tags
 }
