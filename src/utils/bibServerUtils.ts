3d2fe0e1e057de348ad7bc19f0a9603cc50c6aaf
Vera Kahn <verakahn@nypl.org>
2026-08-17 11:44:52 -0400

    add bibserverutils

---
+++ src/utils/bibServerUtils.ts
@@ -0,0 +1,27 @@
+import { NyplSourceMapper } from "@nypl/node-utils"
+
+export function isValidBibId(bibId: string): boolean {
+  const { type, nyplSource, id } = NyplSourceMapper.splitIdentifier(bibId)
+  return !!type && !!nyplSource && !!id
+}
+
+/**
+ * standardizeBibId
+ * Transforms bib id to have lower case prefix (b, hb, cb, pb) and trim check digit
+ */
+export function standardizeBibId(bibId: string): string {
+  // nypl bib ids could have a 9th digit, a check digit which can be 0-9 or x.
+  const nypl = bibId.match(/^([bB])(\d{8})[\dxX]?$/)
+  const princeton = bibId.match(/^([pP][bB])(\d{6,16})$/)
+  const columbia = bibId.match(/^([cC][bB])(\d{6,9})$/)
+  const harvard = bibId.match(/^([hH][bB])(\d{6,18})$/)
+  const matches = [nypl, princeton, columbia, harvard].find(
+    (match) => match?.length === 3
+  )
+  if (matches) {
+    const prefix = matches[1].toLowerCase()
+    const number = matches[2]
+    return prefix + number
+  }
+  return bibId
+}
