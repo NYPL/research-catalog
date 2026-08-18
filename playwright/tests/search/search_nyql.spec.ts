2f54022260f95bb1eaf2c38c6b035506af8e183f
Chris Mulholland <cmulholland@gmail.com>
2026-08-18 15:28:54 -0400

    removed unnecessary test

--- playwright/tests/search/search_nyql.spec.ts
+++ playwright/tests/search/search_nyql.spec.ts
@@ -9,21 +9,6 @@ test.beforeEach(async ({ page }) => {
   await page.goto("")
 })
 
-test.describe("Query Search", () => {
-  test("Do a NYQL query search and assert that results render", async () => {
-    await searchPage.searchFor(searchterm, "Query")
-
-    await expect(searchPage.searchResultsHeading).toBeVisible({
-      timeout: 15000,
-    })
-
-    await expect(async () => {
-      const count = await searchPage.searchResults.count()
-      expect(count).toBeGreaterThan(0)
-    }).toPass({ timeout: 10000 })
-  })
-})
-
 const searchTermForFormatting =
   'title = "journal of paleontology" and date > 2000 and date encloses 1928'
 test.describe("Check for formatting of query search results heading", () => {
